e3eeec118e6280949052fdc1ef6332cfa78dbd03
Sam Hellawell <sshellawell@gmail.com>
2020-03-09 18:59:54 +0000

    remove did module constructor
    Signed-off-by: Sam Hellawell <sshellawell@gmail.com>

--- src/modules/did.js
+++ src/modules/did.js
@@ -1,14 +1,5 @@
 class DIDModule {
   /**
-   * Skeleton constructor, does nothing yet
-   * @constructor
-   * @param {string} test - Who knows
-   */
-  constructor(test) {
-
-  }
-
-  /**
    * Returns a string.
    * @return {string} The foo value.
    */

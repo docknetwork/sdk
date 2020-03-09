cc94c476009695d89f9f6a221d2d05b1798e30e7
Sam Hellawell <sshellawell@gmail.com>
2020-03-09 18:10:18 +0000

    add @polkadot/api dependency and documentation generator

--- src/dock-sdk.js
+++ src/dock-sdk.js
@@ -1,8 +1,17 @@
 class DockSDK {
-  constructor() {
+  /**
+   * Skeleton constructor, does nothing yet
+   * @constructor
+   * @param {string} address - WebSocket Address
+   */
+  constructor(address) {
 
   }
 
+  /**
+   * Returns a string.
+   * @return {string} The bar value.
+   */
   foo() {
     return 'bar';
   }

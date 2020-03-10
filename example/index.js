2015862abc021be7c9350fd533846e0db0161fc6
Sam Hellawell <sshellawell@gmail.com>
2020-03-10 00:30:22 +0000

    minor cleanup
    Signed-off-by: Sam Hellawell <sshellawell@gmail.com>

--- example/index.js
+++ example/index.js
@@ -6,12 +6,13 @@ import DockSDK, {DIDModule} from '../src/dock-sdk';
 
 // Initialise Dock SDK
 const dock = new DockSDK('ws://127.0.0.1:9944');
+const did = '0x2bb29852687a7cbff311dc26e67451';
 
 async function onDIDCreated() {
   console.log('Transaction finalized.');
 
   // Check if DID exists
-  const result = await dock.did.get('0x2bb29852687a7cbff311dc26e67451')
+  const result = await dock.did.get(did)
   console.log('DID:', result && result.toJSON());
   process.exit();
 }
@@ -21,7 +22,6 @@ async function onConnected() {
   const keyring = new Keyring({type: 'sr25519'});
   const account = keyring.addFromUri('//Alice', {name: 'Alice'});
 
-  const did = '0x2bb29852687a7cbff311dc26e67451';
   const controller = '0x666666666666666666666666666666';
   const publicKey = {
     Sr25519: '0x999999999999999999999999999999',

1717ef5e8bcad01e64a79cc7530d3bad9c313234
Sam Hellawell <sshellawell@gmail.com>
2020-03-10 20:40:32 +0000

    did module cleanup

--- src/modules/did.js
+++ src/modules/did.js
@@ -1,6 +1,7 @@
 // TODO: document this file properly
 
-const DockDIDQualifier = "did:dock";
+const DockDIDQualifier = 'did:dock';
+
 class DIDModule {
   constructor(api) {
     this.api = api;
@@ -17,51 +18,57 @@ class DIDModule {
     });
   }
 
+  /**
+   * Gets a DID
+   * @return {object} The DID.
+   */
   async get(did) {
     // TODO: Convert DID and pk to base58
-    var resp = await this.api.query.didModule.dIDs(did);
-    if (!resp) {
-      console.error("Got null response");
-      return;
-    }
-    console.log("response", resp);
-    var detail = resp[0];
-    var lastModifiedBlockNo = resp[1];
-    if (lastModifiedBlockNo == 0) {
-      console.error("Could not find DID:", did);
-    } else {
-      // Create the fully qualified DID like "did:dock:..."
-      var fullyQualifiedDid = `${DockDIDQualifier}:${did}`;
+    const resp = await this.api.query.didModule.dIDs(did);
+    if (resp) {
+      const detail = resp[0];
+      const lastModifiedBlockNo = resp[1];
 
-      // Determine the type of the public key
-      var typ, pk_b58;
-      if (detail.public_key.isSr25519) {
-        typ = "Sr25519VerificationKey2018";
-        pk_b58 = detail.public_key.asSr25519;
-      } else if (detail.public_key.isEd25519) {
-        typ = "Ed25519VerificationKey2018";
-        pk_b58 = detail.public_key.asEd25519;
+      if (lastModifiedBlockNo) {
+        // Create the fully qualified DID like "did:dock:..."
+        const id = `${DockDIDQualifier}:${did}`;
+
+        // Determine the type of the public key
+        let type, publicKeyBase58;
+        if (detail.public_key.isSr25519) {
+          type = 'Sr25519VerificationKey2018';
+          publicKeyBase58 = detail.public_key.asSr25519;
+        } else if (detail.public_key.isEd25519) {
+          type = 'Ed25519VerificationKey2018';
+          publicKeyBase58 = detail.public_key.asEd25519;
+        } else {
+          type = 'EcdsaSecp256k1VerificationKey2019';
+          publicKeyBase58 = detail.public_key.asSecp256K1;
+        }
+
+        // The DID has only one key as of now.
+        const publicKey = [{
+          'id': `${id}#keys-1`,
+          type,
+          'controller': `${DockDIDQualifier}:${detail.controller}`,
+          publicKeyBase58
+        }];
+
+        const authentication = publicKey.map(key => {
+          return key.id;
+        });
+
+        return {
+          '@context': ['https://www.w3.org/ns/did/v1'],
+          id,
+          publicKey,
+          authentication
+        };
       } else {
-        typ = "EcdsaSecp256k1VerificationKey2019";
-        pk_b58 = detail.public_key.asSecp256K1;
+        throw 'Could not find DID: ' + did;
       }
-
-      // The DID has only one key as of now.
-      var didPk = {
-        "id": `${fullyQualifiedDid}#keys-1`,
-        "type": typ,
-        "controller": `${DockDIDQualifier}:${detail.controller}`,
-        "publicKeyBase58": pk_b58
-      };
-
-      return {
-        "@context": ["https://www.w3.org/ns/did/v1"],
-        "id": fullyQualifiedDid,
-        "publicKey": [
-          didPk
-        ],
-        "authentication": [didPk.id]
-      }
+    } else {
+      throw 'Got null response';
     }
   }
 }

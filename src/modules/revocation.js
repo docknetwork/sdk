c8ad9a03663a11c3a2381dbbcdfcdf01562ff79b
Sam Hellawell <sshellawell@gmail.com>
2020-03-13 18:49:10 +0000

    add dummy revoke/unrevoke methods

--- src/modules/revocation.js
+++ src/modules/revocation.js
@@ -8,6 +8,28 @@ class RevocationModule {
   constructor(api) {
     this.api = api;
   }
+
+  /**
+   * Revoke credentials
+   * @param {string} origin - The origin
+   * @param {Revoke} to_revoke - contains the credentials to be revoked
+   * @param {Array} controllers - contains the `(DID, signature)`s of the controllers who are allowing these revocations. Each tuple contains the controller and its signature.
+   * @return {Extrinsic} The extrinsic to sign and send.
+   */
+  revoke(origin, to_revoke, controllers) {
+
+  }
+
+  /**
+   * Unrevoke credentials
+   * @param {string} origin - The origin
+   * @param {Unrevoke} to_unrevoke - contains the credentials to be revoked
+   * @param {Array} controllers - contains the `(DID, signature)`s of the controllers who are allowing these unrevocations. Each tuple contains the controller and its signature.
+   * @return {Extrinsic} The extrinsic to sign and send.
+   */
+  unrevoke(origin, to_unrevoke, controllers) {
+    
+  }
 }
 
 export default RevocationModule;

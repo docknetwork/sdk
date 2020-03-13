cba6fe6a6876db48d3d4cf1d8e00a4089ed1648f
lovesh <lovesh.bond@gmail.com>
2020-03-13 14:03:07 +0530

    Avoid type conflict, change DID get method as per the node and remove unneccessary fields from enum representation
    Signed-off-by: lovesh <lovesh.bond@gmail.com>

--- example/index.js
+++ example/index.js
@@ -6,13 +6,13 @@ import DockSDK, {DIDModule} from '../src/dock-sdk';
 
 // Initialise Dock SDK
 const dock = new DockSDK('ws://127.0.0.1:9944');
-const did = '0x2bb29852687a7cbff311dc26e67451';
+const did = '0x2bb29852687a7cbff311dc26e67451332bb29852687a7cbff311dc26e6745133';
 
 async function onDIDCreated() {
   console.log('Transaction finalized.');
 
   // Check if DID exists
-  const result = await dock.did.get(did)
+  const result = await dock.did.get(did);
   console.log('DID:', result);
   process.exit();
 }
@@ -22,11 +22,9 @@ async function onConnected() {
   const keyring = new Keyring({type: 'sr25519'});
   const account = keyring.addFromUri('//Alice', {name: 'Alice'});
 
-  const controller = '0x666666666666666666666666666666';
+  const controller = '0x6666666666666666666666666666666666666666666666666666666666666666';
   const publicKey = {
-    Sr25519: '0x999999999999999999999999999999',
-    Ed25519: undefined,
-    Secp256k1: undefined,
+    Sr25519: '0x9999999999999999999999999999999999999999999999999999999999999999',
   };
 
 

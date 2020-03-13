d81df0761cad661f9f1bfb51e45843365cec8ba5
Sam Hellawell <sshellawell@gmail.com>
2020-03-13 19:12:57 +0000

    lint
    Signed-off-by: Sam Hellawell <sshellawell@gmail.com>

--- src/modules/revocation.js
+++ src/modules/revocation.js
@@ -17,59 +17,59 @@ class RevocationModule {
    * @return {Extrinsic} The extrinsic to sign and send.
    */
   new(origin, id, registry) {
-    
+    return this.api.tx.revocationModule.new(origin, id, registry);
   }
 
   /**
    * Revoke credentials
    * @param {string} origin - The origin
-   * @param {Revoke} to_revoke - contains the credentials to be revoked
+   * @param {Revoke} toRevoke - contains the credentials to be revoked
    * @param {Array} controllers - contains the `(DID, signature)`s of the controllers who are allowing these revocations. Each tuple contains the controller and its signature.
    * @return {Extrinsic} The extrinsic to sign and send.
    */
-  revoke(origin, to_revoke, controllers) {
-
+  revoke(origin, toRevoke, controllers) {
+    return this.api.tx.revocationModule.revoke(origin, toRevoke, controllers);
   }
 
   /**
    * Unrevoke credentials
    * @param {string} origin - The origin
-   * @param {Unrevoke} to_unrevoke - contains the credentials to be revoked
+   * @param {Unrevoke} toUnrevoke - contains the credentials to be revoked
    * @param {Array} controllers - contains the `(DID, signature)`s of the controllers who are allowing these unrevocations. Each tuple contains the controller and its signature.
    * @return {Extrinsic} The extrinsic to sign and send.
    */
-  unrevoke(origin, to_unrevoke, controllers) {
-
+  unrevoke(origin, toUnrevoke, controllers) {
+    return this.api.tx.revocationModule.unrevoke(origin, toUnrevoke, controllers);
   }
 
   /**
    * Deleting revocation registry
    * @param {string} origin - The origin
-   * @param {RemoveRegistry} to_remove - contains the registry to remove
+   * @param {RemoveRegistry} toRemove - contains the registry to remove
    * @param {Array} controllers - contains the `(DID, signature)`s of the controllers who are allowing this removal. Each tuple contains the controller and its signature
    * @return {Extrinsic} The extrinsic to sign and send.
    */
-  remove(origin, to_remove, controllers) {
-
+  remove(origin, toRemove, controllers) {
+    return this.api.tx.revocationModule.remove(origin, toRemove, controllers);
   }
 
   /**
    * The read-only call get_revocation_registry is used to get details of the revocation registry like controllers, policy and type. If the registry is not present, None is returned.
-   * @param {string} rev_reg_id - Revocation registry ID
+   * @param {string} revRegID - Revocation registry ID
    * @return {Extrinsic} The extrinsic to sign and send.
    */
-  getRevocationRegistry(rev_reg_id) {
-
+  getRevocationRegistry(revRegID) {
+    return this.api.tx.revocationModule.getRevocationRegistry(revRegID);
   }
 
   /**
    * The read-only call get_revocation_status is used to check whether a credential is revoked or not and does not consume any tokens. If
-   * @param {string} rev_reg_id - Revocation registry ID
-   * @param {string} cred_id - Credential ID
+   * @param {string} revRegID - Revocation registry ID
+   * @param {string} credentialID - Credential ID
    * @return {Extrinsic} The extrinsic to sign and send.
    */
-  getRevocationStatus(rev_reg_id, cred_id) {
-
+  getRevocationStatus(revRegID, credentialID) {
+    return this.api.tx.revocationModule.getRevocationStatus(revRegID, credentialID);
   }
 }
 

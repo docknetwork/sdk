4b17117a70d1e4152b8f1d1316464fb8be3679f8
Fausto Woelflin <faustow@gmail.com>
2020-03-13 20:16:29 -0300

    Add verifiable credential module.

---
+++ src/modules/vc.js
@@ -0,0 +1,23 @@
+/** Class to create, update and sign Verifiable Claims */
+class VCModule {
+
+
+  issue(options = {}) {
+
+  }
+
+  verify(options = {}) {
+
+  }
+
+  createPresentation({verifiableCredential, id, holder} = {}) {
+
+  }
+
+  signPresentation(options = {}) {
+
+  }
+
+}
+
+export default VCModule;

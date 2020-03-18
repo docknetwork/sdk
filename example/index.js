3e09b01733a754c78acb2282bf37ab19949c2e3c
lovesh <lovesh.bond@gmail.com>
2020-03-19 00:14:44 +0530

    try gloabal keyring
    Signed-off-by: lovesh <lovesh.bond@gmail.com>

--- example/index.js
+++ example/index.js
@@ -9,6 +9,8 @@ import {u8aToHex, hexToU8a} from '@polkadot/util';
 import dock from '../src/dock-sdk';
 import {PublicKeySr25519, PublicKeyEd25519, SignatureSr25519} from '../src/dock-sdk';
 
+const globalKeyring = new Keyring({ type: 'sr25519' });
+
 // The script will create a new DID with a key, then update the key to another one and then remove the DID
 
 // Generate a random DID
@@ -26,19 +28,20 @@ const secondKeySeed = randomAsHex(32);
 // This function assumes the DID has been written.
 async function postDIDKeyUpdated() {
   console.log('Removing DID now.');
-  const keyring2 = new Keyring();
-  const account = keyring2.addFromUri('//Bob', {name: 'Bob'});
+  //const keyring2 = new Keyring({type: 'sr25519'});
+  const account = globalKeyring.addFromUri('//Bob', {name: 'Bob'});
 
   // Get DID details. This call will fail if DID is not written already
-  const [detail, last_modified_in_block] = await dock.did.getDetail(didIdentifier);
+  const last_modified_in_block = await dock.did.getDetail(didIdentifier)[1];
 
-  const keyring = new Keyring();
+  //const keyring = new Keyring();
   // Sign key update with this key pair as this is the current key of the DID
-  const currentPair = keyring.addFromUri(firstKeySeed, null, 'sr25519');
+  const currentPair = globalKeyring.addFromUri(firstKeySeed, null, 'sr25519');
 
-  const serializedKeyUpdate = dock.did.getSerializedDIDRemoval(didIdentifier, last_modified_in_block);
+  const serializedDIDRemoval = dock.did.getSerializedDIDRemoval(didIdentifier, last_modified_in_block);
   console.log(last_modified_in_block);
-  const signature = new SignatureSr25519(u8aToHex(currentPair.sign(serializedKeyUpdate)));
+  const signature = new SignatureSr25519(u8aToHex(currentPair.sign(serializedDIDRemoval)));
+  //const signature = new SignatureSr25519('0xd0b107896083ff8235d8fdab35247203e03f65c5e67c2df9204a1a0ce4658f78d0b107896083ff8235d8fdab35247203e03f65c5e67c2df9204a1a0ce4658f78');
 
   const transaction = dock.did.remove(didIdentifier, signature, last_modified_in_block);
   console.log(transaction);
@@ -87,12 +90,12 @@ async function onDIDCreated() {
 
 // Called when connected to the node
 async function onConnected() {
-  const keyring = new Keyring({type: 'sr25519'});
-  const account = keyring.addFromUri('//Alice', {name: 'Alice'});
+  //const keyring = new Keyring({type: 'sr25519'});
+  const account = globalKeyring.addFromUri('//Alice', {name: 'Alice'});
 
   const controller = randomAsHex(32);
 
-  const firstPair = keyring.addFromUri(firstKeySeed, null, 'sr25519');
+  const firstPair = globalKeyring.addFromUri(firstKeySeed, null, 'sr25519');
   const publicKey = new PublicKeySr25519(u8aToHex(firstPair.publicKey));
 
   console.log('Submitting new DID', didIdentifier, controller, publicKey);

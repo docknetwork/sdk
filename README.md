586c87a6e42ffb39e85bcc4eb7051be61762871e
lovesh <lovesh.bond@gmail.com>
2020-03-19 17:58:57 +0530

    add test for util
    Signed-off-by: lovesh <lovesh.bond@gmail.com>

--- README.md
+++ README.md
@@ -29,6 +29,10 @@ Running dev node:
 ./target/release/dock-testnet --base-path /tmp/alice --chain local --alice --port 30333 --ws-port 9944 --rpc-port 9933 --telemetry-url ws://telemetry.polkadot.io:1024 --validator --dev
 ```
 
+## Test
+- Run all tests with `yarn test`
+- Run a single test module with `yarn test <module name or prefix>`
+
 ## Basic SDK example
 
 Simply run
---
+++ tests/utils.test.js
@@ -0,0 +1,39 @@
+import {
+  isHexWithGivenByteSize
+} from '../src/utils';
+
+describe('Utils SDK', () => {
+  
+  test('isHexWithGivenByteSize rejects strings not starting with 0x', () => {
+    expect(isHexWithGivenByteSize('12')).toBe(false);
+  });
+
+  test('isHexWithGivenByteSize rejects strings with invalid hex', () => {
+    expect(isHexWithGivenByteSize('0x1h')).toBe(false);
+  });
+
+  test('isHexWithGivenByteSize rejects strings with non-full byte', () => {
+    expect(isHexWithGivenByteSize('0x123')).toBe(false);
+  });
+
+  test('isHexWithGivenByteSize rejects strings with byte size 0', () => {
+    expect(isHexWithGivenByteSize('0x')).toBe(false);
+  });
+
+  test('isHexWithGivenByteSize rejects strings not matching expected byte size', () => {
+    expect(isHexWithGivenByteSize('0x12', 2)).toBe(false);
+    expect(isHexWithGivenByteSize('0x1234', 1)).toBe(false);
+  });
+
+  test('isHexWithGivenByteSize accepts correct hex string with full bytes', () => {
+    expect(isHexWithGivenByteSize('0x12')).toBe(true);
+    expect(isHexWithGivenByteSize('0x1234')).toBe(true);
+    expect(isHexWithGivenByteSize('0x1234ef')).toBe(true);
+  });
+
+  test('isHexWithGivenByteSize accepts correct hex string matching expected byte size', () => {
+    expect(isHexWithGivenByteSize('0x12', 1)).toBe(true);
+    expect(isHexWithGivenByteSize('0x1234', 2)).toBe(true);
+    expect(isHexWithGivenByteSize('0x1234ef', 3)).toBe(true);
+  });
+});

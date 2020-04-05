3a1e5ecdde8d54932213645841b6400073e24790
lovesh <lovesh.bond@gmail.com>
2020-04-06 03:44:54 +0530

    Credential with Sr25519 sig. Verification failure
    Signed-off-by: lovesh <lovesh.bond@gmail.com>

--- tests/integration/issuing.test.js
+++ tests/integration/issuing.test.js
@@ -11,7 +11,7 @@ import Resolver from '../../src/resolver';
 
 import {FullNodeEndpoint, TestKeyringOpts, TestAccount} from '../test-constants';
 import {registerNewDIDUsingPair} from './helpers';
-import {generateEcdsaSecp256k1Keypair} from '../../src/utils/misc';
+import {generateEcdsaSecp256k1Keypair, getPublicKeyFromKeyringPair} from '../../src/utils/misc';
 import Secp256k1KeyPair  from 'secp256k1-key-pair';
 import {issueCredential, verifyCredential} from '../../src/utils/vc';
 
@@ -27,6 +27,10 @@ const issuer2DID = createNewDockDID();
 const issuer2KeyPers = 'issuer2';
 const issuer2KeyEntropy = randomAsHex(32);
 
+const issuer3DID = createNewDockDID();
+// seed used for 3nd issuer keys
+const issuer3KeySeed = randomAsHex(32);
+
 const holderDID = createNewDockDID();
 
 const credId = randomAsHex(32);
@@ -34,7 +38,9 @@ const credId = randomAsHex(32);
 const unsignedCred = {
   '@context': [
     'https://www.w3.org/2018/credentials/v1',
-    'https://www.w3.org/2018/credentials/examples/v1'
+    // Following URL is for Sr25519 Signature and verification key
+    'https://gist.githubusercontent.com/lovesh/4e2b49c9d6588dedcff46aaa6f1a1cad/raw',
+    'https://www.w3.org/2018/credentials/examples/v1',
   ],
   id: credId,
   type: ['VerifiableCredential', 'AlumniCredential'],
@@ -65,6 +71,7 @@ function getIssuerKeyDoc(did, keypair, typ) {
 /**
  * Test helper to get the matching doc as per the cred
  * @param cred - credential to match
+ * @param issuer
  * @param issuerKeyId
  * @param sigType
  * @returns {{issuanceDate: string, credentialSubject: {alumniOf: string, id: *}, id: *, proof: AsymmetricMatcher, type: [string, string], issuer: string}}
@@ -121,6 +128,9 @@ describe('Verifiable Credential issuance where issuer has a Dock DID', () => {
     const pair2 = generateEcdsaSecp256k1Keypair(issuer2KeyPers, issuer2KeyEntropy);
     await registerNewDIDUsingPair(dock, issuer2DID, pair2);
 
+    const pair3 = dock.keyring.addFromUri(issuer3KeySeed, null, 'sr25519');
+    await registerNewDIDUsingPair(dock, issuer3DID, pair3);
+
     const providers = {
       'dock': FullNodeEndpoint,
     };
@@ -164,6 +174,43 @@ describe('Verifiable Credential issuance where issuer has a Dock DID', () => {
       )
     );
     const result = await verifyCredential(credential, resolver);
+    expect(result).toMatchObject(
+      expect.objectContaining(
+        getProofMatcherDoc()
+      )
+    );
+  }, 40000);
+
+  test('Issue a verifiable credential with sr25519 key and verify it', async () => {
+    const issuerKeyPair = dock.keyring.addFromUri(issuer3KeySeed, null, 'sr25519');
+    const issuerKey = {
+      id: `${issuer3DID}#keys-1`,
+      controller: issuer3DID,
+      type: 'Sr25519VerificationKey2020',
+      keypair: issuerKeyPair,
+      publicKey: getPublicKeyFromKeyringPair(issuerKeyPair)
+    };
+    const credential = await vc.issueCredential(issuerKey, unsignedCred);
+    console.log(credential);
+
+    // XXX: Temporary fix.
+    // TODO: Add Sr25519Signature2020 to acceptable security contexts
+    expect(credential.proof.type).toBe('/Sr25519Signature2020');
+    credential.proof = {
+      type: 'Sr25519Signature2020',
+      created: credential.proof['dct:created']['@value'],
+      jws: credential.proof['https://w3id.org/security#jws'],
+      proofPurpose: 'assertionMethod',
+      verificationMethod: credential.proof['https://w3id.org/security#verificationMethod']['id']
+    };
+
+    expect(credential).toMatchObject(
+      expect.objectContaining(
+        getCredMatcherDoc(unsignedCred, issuer3DID, issuerKey.id, 'Sr25519Signature2020')
+      )
+    );
+    const result = await vc.verifyCredential(credential, resolver);
+    console.log(result);
     expect(result).toMatchObject(
       expect.objectContaining(
         getProofMatcherDoc()

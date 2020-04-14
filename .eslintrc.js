b02d7974c6c1ebc582044a0d7f72ab55e35cfde3
Sam Hellawell <sshellawell@gmail.com>
2020-04-14 19:51:06 +0100

    use airbnb-base eslint config
    Signed-off-by: Sam Hellawell <sshellawell@gmail.com>

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,32 +7,9 @@ module.exports = {
     "browser": true,
     "es6": true
   },
-  "extends": ["eslint:recommended", "plugin:jest/recommended"],
+  "extends": ["eslint:recommended", "plugin:jest/recommended", "airbnb-base"],
   "parserOptions": {
     "ecmaVersion": 2018,
     "sourceType": "module"
-  },
-  "rules": {
-    "space-before-function-paren": ["error", {
-      "anonymous": "never",
-      "named": "never",
-      "asyncArrow": "always"
-    }],
-    "indent": [
-      "error",
-      2
-    ],
-    "linebreak-style": [
-      "error",
-      "unix"
-    ],
-    "quotes": [
-      "error",
-      "single"
-    ],
-    "semi": [
-      "error",
-      "always"
-    ]
   }
 };

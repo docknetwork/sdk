e5bd2f7e26f1a48f450f83cd843f1d15212fe7eb
lovesh <lovesh.bond@gmail.com>
2020-05-26 23:33:33 +0530

    Update polkadot-js version and temporarily expand refs
    Signed-off-by: lovesh <lovesh.bond@gmail.com>

--- src/utils/vc/schemas/immunity_event_record.js
+++ src/utils/vc/schemas/immunity_event_record.js
@@ -1,4 +1,6 @@
-export default {
+import proofOfHealthCoreSchema from './proof_of_health_core';
+
+const schema = {
   $schema: 'http://json-schema.org/draft-07/schema#',
   description: 'Show if immunity by vaccination or testing',
   type: 'object',
@@ -9,14 +11,6 @@ export default {
     },
   },
   properties: {
-    // TODO: Change the id once blob integration is done in SDK
-    firstName: { $ref: '/blob:dock:0x123/firstName' },
-    firstInitial: { $ref: '/blob:dock:0x123/firstInitial' },
-    lastName: { $ref: '/blob:dock:0x123/lastName' },
-    lastInitial: { $ref: '/blob:dock:0x123/lastInitial' },
-    yearOfBirth: { $ref: '/blob:dock:0x123/yearOfBirth' },
-    photo: { $ref: '/blob:dock:0x123/photo' },
-    biometricTemplate: { $ref: '/blob:dock:0x123/biometricTemplate' },
     eventType: {
       type: 'string',
       enum: ['vaccination', 'antibody test'],
@@ -43,3 +37,8 @@ export default {
   required: ['firstName', 'firstInitial', 'lastName', 'lastInitial', 'photo', 'biometricTemplate', 'yearOfBirth',
     'eventType', 'eventTime', 'eventBy', 'eventFacility', 'eventName', 'potencyDate'],
 };
+
+// TODO: Use `$ref` instead. Some of the changes already done in other branch.
+const properties = { ...proofOfHealthCoreSchema.properties, ...schema.properties };
+schema.properties = properties;
+export default schema;
--- src/utils/vc/schemas/infection_diagnosis.js
+++ src/utils/vc/schemas/infection_diagnosis.js
@@ -1,4 +1,6 @@
-export default {
+import proofOfHealthCoreSchema from './proof_of_health_core';
+
+const schema = {
   $schema: 'http://json-schema.org/draft-07/schema#',
   // TODO: Use a proper id once blob integration is done in SDK
   description: 'Diagnosis with viral infection on a particular date',
@@ -10,14 +12,6 @@ export default {
     },
   },
   properties: {
-    // TODO: Change the id once blob integration is done in SDK
-    firstName: { $ref: '/blob:dock:0x123/firstName' },
-    firstInitial: { $ref: '/blob:dock:0x123/firstInitial' },
-    lastName: { $ref: '/blob:dock:0x123/lastName' },
-    lastInitial: { $ref: '/blob:dock:0x123/lastInitial' },
-    yearOfBirth: { $ref: '/blob:dock:0x123/yearOfBirth' },
-    photo: { $ref: '/blob:dock:0x123/photo' },
-    biometricTemplate: { $ref: '/blob:dock:0x123/biometricTemplate' },
     diagnosisCode: {
       type: 'string',
       enum: ['CodeA', 'CodeB', 'CodeC'],
@@ -50,3 +44,8 @@ export default {
   required: ['firstName', 'firstInitial', 'lastName', 'lastInitial', 'photo', 'biometricTemplate', 'yearOfBirth',
     'diagnosisCode', 'diagnosisTime', 'diagnosisLocation', 'diagnosedBy', 'diagnosisFacility', 'diagnosisMethods', 'declaredSafeDate'],
 };
+
+// TODO: Use `$ref` instead. Some of the changes already done in other branch.
+const properties = { ...proofOfHealthCoreSchema.properties, ...schema.properties };
+schema.properties = properties;
+export default schema;

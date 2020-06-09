59b8bcf50885f7258a4bd0d68456a5102e8e8334
lovesh <lovesh.bond@gmail.com>
2020-06-10 03:16:33 +0530

    Script to get the DID doc for a DID
    Signed-off-by: lovesh <lovesh.bond@gmail.com>

---
+++ scripts/get_did_doc.js
@@ -0,0 +1,30 @@
+// Import Dock API
+import dock from '../src/api';
+
+// The following can be tweaked depending on where the node is running and what
+// account is to be used for sending the transaction.
+import { FullNodeEndpoint } from '../tests/test-constants';
+
+async function getDIDDoc(dockDID) {
+  console.log('Getting DID now.');
+  // Check if DID exists
+  const result = await dock.did.getDocument(dockDID);
+  console.log('DID Document:', JSON.stringify(result, null, 2));
+  process.exit(0);
+}
+
+if (process.argv.length !== 3) {
+  console.error('Need one and only one argument as the DID', process.argv);
+  process.exit(2);
+}
+
+dock.init({
+  address: FullNodeEndpoint,
+})
+  .then(() => {
+    return getDIDDoc(process.argv[2]);
+  })
+  .catch((error) => {
+    console.error('Error occurred somewhere, it was caught!', error);
+    process.exit(1);
+  });

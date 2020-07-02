5bea7fbaea9c584a8b9fbb0327389c29633c248f
Sam Hellawell <sshellawell@gmail.com>
2020-07-02 22:25:12 +0100

    simplify cred-revocation expandedStatusProperty
    Signed-off-by: Sam Hellawell <sshellawell@gmail.com>

--- tests/unit/cred-revocation.test.js
+++ tests/unit/cred-revocation.test.js
@@ -5,23 +5,23 @@ import {
 
 describe('Check isRevocationCheckNeeded', () => {
   test('isRevocationCheckNeeded returns true when credentialStatus is present and forceRevocationCheck is true and revocation API is not given', () => {
-    expect(isRevocationCheckNeeded({ [`${expandedStatusProperty}`]: {} }, true, undefined)).toBe(true);
+    expect(isRevocationCheckNeeded({ [expandedStatusProperty]: {} }, true, undefined)).toBe(true);
   });
 
   test('isRevocationCheckNeeded returns true when credentialStatus is present and forceRevocationCheck is true and revocation API is given', () => {
-    expect(isRevocationCheckNeeded({ [`${expandedStatusProperty}`]: {} }, true, { dock: 'Anything' })).toBe(true);
+    expect(isRevocationCheckNeeded({ [expandedStatusProperty]: {} }, true, { dock: 'Anything' })).toBe(true);
   });
 
   test('isRevocationCheckNeeded returns true when credentialStatus is present and forceRevocationCheck is false but revocation API is given', () => {
-    expect(isRevocationCheckNeeded({ [`${expandedStatusProperty}`]: {} }, false, { dock: 'Anything' })).toBe(true);
+    expect(isRevocationCheckNeeded({ [expandedStatusProperty]: {} }, false, { dock: 'Anything' })).toBe(true);
   });
 
   test('isRevocationCheckNeeded returns true when credentialStatus is present and forceRevocationCheck is false but revocation API is empty object', () => {
-    expect(isRevocationCheckNeeded({ [`${expandedStatusProperty}`]: {} }, false, {})).toBe(true);
+    expect(isRevocationCheckNeeded({ [expandedStatusProperty]: {} }, false, {})).toBe(true);
   });
 
   test('isRevocationCheckNeeded returns false when credentialStatus is present and forceRevocationCheck is false and revocation API is not given', () => {
-    expect(isRevocationCheckNeeded({ [`${expandedStatusProperty}`]: {} }, false, undefined)).toBe(false);
+    expect(isRevocationCheckNeeded({ [expandedStatusProperty]: {} }, false, undefined)).toBe(false);
   });
 
   test('isRevocationCheckNeeded returns false when credentialStatus is not present and forceRevocationCheck is true and revocation API is not given', () => {

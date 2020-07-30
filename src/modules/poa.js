67c1ca450da6f388ffe60224a4481656ac539e6b
lovesh <lovesh.bond@gmail.com>
2020-07-31 03:26:02 +0530

    Allow to sent by sudo
    Signed-off-by: lovesh <lovesh.bond@gmail.com>

--- src/modules/poa.js
+++ src/modules/poa.js
@@ -21,142 +21,167 @@ class PoAModule {
   }
 
   /**
-   * Set session key for a validator
+   * Create extrinsic to set session key for a validator
    * @param {string} validatorAddress - validator's account
    * @param {string} sessionKey - session key of the validator
+   * @param {boolean} asSudo - The extrinsic needs to be sent by sudo.
    * @return {object} The extrinsic to sign and send.
    */
-  setSessionKey(validatorAddress, sessionKey) {
+  setSessionKey(validatorAddress, sessionKey, asSudo = false) {
     if (!isHexWithGivenByteSize(sessionKey, SessionKeyByteSize)) {
       throw new Error(`Session key must be in hex and of ${SessionKeyByteSize} bytes`);
     }
-    return this.module.setSessionKey(validatorAddress, sessionKey);
+    const txn = this.module.setSessionKey(validatorAddress, sessionKey);
+    return this.asSudoIfNeeded(txn, asSudo);
   }
 
   /**
-   * Set maximum active validators
+   * Create extrinsic to set maximum active validators
    * @param {number} count - The number of allowed validators
+   * @param {boolean} asSudo - The extrinsic needs to be sent by sudo.
    * @returns {object} The extrinsic to sign and send.
    */
-  setMaxActiveValidators(count) {
+  setMaxActiveValidators(count, asSudo = false) {
     if (!Number.isInteger(count) || count < 1 || count > MaxActiveValidators) {
       throw new Error(`Count must an integer between 1 and ${MaxActiveValidators}`);
     }
-    return this.module.setMaxActiveValidators(count);
+    const txn = this.module.setMaxActiveValidators(count);
+    return this.asSudoIfNeeded(txn, asSudo);
   }
 
   // The following 2 methods can be combined into one by making them accept a boolean argument but
   // as they are potentially dangerous operations, being very explicit.
 
   /**
-   * Enable emission rewards.
+   * Create extrinsic to enable emission rewards.
+   * @param {boolean} asSudo - The extrinsic needs to be sent by sudo.
    * @returns {object} The extrinsic to sign and send.
    */
-  enableEmissions() {
-    return this.module.setEmissionStatus(true);
+  enableEmissions(asSudo = false) {
+    const txn = this.module.setEmissionStatus(true);
+    return this.asSudoIfNeeded(txn, asSudo);
   }
 
   /**
-   * Disable emission rewards.
+   * Create extrinsic to disable emission rewards.
+   * @param {boolean} asSudo - The extrinsic needs to be sent by sudo.
    * @returns {object} The extrinsic to sign and send.
    */
-  disableEmissions() {
-    return this.module.setEmissionStatus(false);
+  disableEmissions(asSudo = false) {
+    const txn = this.module.setEmissionStatus(false);
+    return this.asSudoIfNeeded(txn, asSudo);
   }
 
   /**
-   * Set minimum epoch length in terms of slots
+   * Create extrinsic to set minimum epoch length in terms of slots
    * @param {number} length - Number of slots
+   * @param {boolean} asSudo - The extrinsic needs to be sent by sudo.
    * @returns {object} The extrinsic to sign and send.
    */
-  setMinEpochLength(length) {
+  setMinEpochLength(length, asSudo = false) {
     if (!Number.isInteger(length) || length < 1) {
       throw new Error('length must an integer > 0');
     }
-    return this.module.setMinEpochLength(length);
+    const txn = this.module.setMinEpochLength(length);
+    return this.asSudoIfNeeded(txn, asSudo);
   }
 
   /**
-   * Set maximum emission rewards per validator per epoch
+   * Create extrinsic to set maximum emission rewards per validator per epoch
    * @param {number} rewards - Amount of rewards
+   * @param {boolean} asSudo - The extrinsic needs to be sent by sudo.
    * @returns {object} The extrinsic to sign and send.
    */
-  setMaxEmissionRewardsPerValidatorForEpoch(rewards) {
+  setMaxEmissionRewardsPerValidatorForEpoch(rewards, asSudo = false) {
     if (!Number.isInteger(rewards) || rewards < 0) {
       throw new Error('Rewards must be a positive integer');
     }
-    return this.module.setMaxEmmValidatorEpoch(rewards);
+    const txn = this.module.setMaxEmmValidatorEpoch(rewards);
+    return this.asSudoIfNeeded(txn, asSudo);
   }
 
   /**
-   * Set lock percentage of validator's emission rewards per epoch.
+   * Create extrinsic to set lock percentage of validator's emission rewards per epoch.
    * @param {number} lockPercent - Percentage of validator rewards
-   * @returns {*}
+   * @param {boolean} asSudo - The extrinsic needs to be sent by sudo.
+   * @returns {object} The extrinsic to sign and send.
    */
-  setValidatorRewardLockPercent(lockPercent) {
+  setValidatorRewardLockPercent(lockPercent, asSudo = false) {
     if (!Number.isInteger(lockPercent) || lockPercent < 0 || lockPercent > 100) {
       throw new Error('Lock percentage must a positive integer in [0, 100]');
     }
-    return this.module.setValidatorRewardLockPc(lockPercent);
+    const txn = this.module.setValidatorRewardLockPc(lockPercent);
+    return this.asSudoIfNeeded(txn, asSudo);
   }
 
   /**
-   * Set treasury's emission reward percentage per epoch.
+   * Create extrinsic to set treasury's emission reward percentage per epoch.
    * @param {number} rewardPercent - Percentage of epoch reward
-   * @returns {*}
+   * @param {boolean} asSudo - The extrinsic needs to be sent by sudo.
+   * @returns {object} The extrinsic to sign and send.
    */
-  setTreasuryRewardPercent(rewardPercent) {
+  setTreasuryRewardPercent(rewardPercent, asSudo = false) {
     if (!Number.isInteger(rewardPercent) || rewardPercent < 0 || rewardPercent > 100) {
       throw new Error('Reward percentage must a positive integer in [0, 100]');
     }
-    return this.module.setTreasuryRewardPc(rewardPercent);
+    const txn = this.module.setTreasuryRewardPc(rewardPercent);
+    return this.asSudoIfNeeded(txn, asSudo);
   }
 
   /**
-   * Transfer funds from treasury to the recipient.
+   * Create extrinsic to transfer funds from treasury to the recipient.
    * @param {string} recipientAddress - Address of the recipient
    * @param {number} amount
+   * @param {boolean} asSudo - The extrinsic needs to be sent by sudo.
    * @returns {object} The extrinsic to sign and send.
    */
-  withDrawFromTreasury(recipientAddress, amount) {
+  withDrawFromTreasury(recipientAddress, amount, asSudo = false) {
     if (!Number.isInteger(amount) || amount < 1) {
       throw new Error('Amount must be a positive integer > 0');
     }
-    return this.module.withdrawFromTreasury(recipientAddress, amount);
+    const txn = this.module.withdrawFromTreasury(recipientAddress, amount);
+    return this.asSudoIfNeeded(txn, asSudo);
   }
 
   /**
-   * Add a new validator.
+   * Create extrinsic to add a new validator.
    * @param {string} validatorAddress - Address of the validator
    * @param {bool} shortCircuit - If true, don't wait till the end of epoch to add else wait for current epoch to finish
+   * @param {boolean} asSudo - The extrinsic needs to be sent by sudo.
    * @returns {object} The extrinsic to sign and send.
    */
-  addValidator(validatorAddress, shortCircuit) {
-    return this.module.addValidator(validatorAddress, shortCircuit);
+  addValidator(validatorAddress, shortCircuit, asSudo = false) {
+    const txn = this.module.addValidator(validatorAddress, shortCircuit);
+    return this.asSudoIfNeeded(txn, asSudo);
   }
 
   /**
-   * Remove an existing validator.
+   * Create extrinsic to remove an existing validator.
    * @param {string} validatorAddress - Address of the validator
    * @param {bool} shortCircuit - If true, don't wait till the end of epoch to add else wait for current epoch to finish
+   * @param {boolean} asSudo - The extrinsic needs to be sent by sudo.
    * @returns {object} The extrinsic to sign and send.
    */
-  removeValidator(validatorAddress, shortCircuit) {
-    return this.module.removeValidator(validatorAddress, shortCircuit);
+  removeValidator(validatorAddress, shortCircuit, asSudo = false) {
+    const txn = this.module.removeValidator(validatorAddress, shortCircuit);
+    return this.asSudoIfNeeded(txn, asSudo);
   }
 
   /**
-   * Swap an existing validator for a new one. Takes effect immediately.
+   * Create extrinsic to swap an existing validator for a new one. Takes effect immediately.
    * @param {string} swapOutValidator - Address of the validator to remove
    * @param {string} swapInValidator - Address of the validator to add
+   * @param {boolean} asSudo - The extrinsic needs to be sent by sudo.
    * @returns {object} The extrinsic to sign and send.
    */
-  swapValidator(swapOutValidator, swapInValidator) {
-    return this.module.swapValidator(swapOutValidator, swapInValidator);
+  swapValidator(swapOutValidator, swapInValidator, asSudo = false) {
+    const txn = this.module.swapValidator(swapOutValidator, swapInValidator);
+    return this.asSudoIfNeeded(txn, asSudo);
   }
 
   /**
-   * Get the free and reserved balance of the account. Useful when checking unlocked and locked balance of the validator.
+   * Create extrinsic to get the free and reserved balance of the account. Useful when checking unlocked and
+   * locked balance of the validator.
    * @param {string} account
    * @returns {Promise<Array>} - A 2-element array where the first is the free (unlocked) balance and the second is the
    * reserved (locked) balance.
@@ -164,6 +189,20 @@ class PoAModule {
   async getBalance(account) {
     const { data: balance } = await this.api.query.system.account(account);
     return [balance.free.toHex(), balance.reserved.toHex()];
+  }
+
+  /**
+   * Takes an extrinsic and returns a modified extrinsic if it needs to be sent by sudo otherwise returns the given
+   * extrinsic as it is
+   * @param {object} txn - The extrinsic to send
+   * @param {boolean} asSudo - If the extrinsic needs to be sent by sudo
+   * @returns {object} The extrinsic to sign and send.
+   */
+  asSudoIfNeeded(txn, asSudo = false) {
+    if (asSudo) {
+      return this.api.tx.sudo.sudo(txn);
+    }
+    return txn;
   }
 }
 

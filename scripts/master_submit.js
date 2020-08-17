b4cc30771f40cb72fd001919b94c93d01354aa5b
Andrew Dirksen <andrew@dirksen.com>
2020-08-17 15:58:47 -0700

    outline master voting scripts

---
+++ scripts/master_submit.js
@@ -0,0 +1,16 @@
+// Takes a proposal and a proof of master authorization and submits the proposal as a
+// Master::execute() transaction.
+
+async function main() {
+  // get secret key for submitting
+
+  // encode proposal as call
+
+  // would be nice: verify votes are valid before submitting
+
+  // combine signatures and encoded call into a single "execute" extrinsic
+
+  // sign and submit
+}
+
+await main();

29882d20146f281c973dab7abd876d41afd762be
Andrew Dirksen <andrew@dirksen.com>
2020-08-25 15:30:14 -0700

    add argument parsing to ./scripts/change_did_key.js

--- scripts/change_did_key.js
+++ scripts/change_did_key.js
@@ -1,21 +1,41 @@
+/// This script assumes that all keys are sr25519.
+///
+/// Warning: This script only supports sr25519 keys. Do not use it to set an non-sr25519 public key.
+///          If you do, you will lose control of your DID.
+
 import { Keyring } from '@polkadot/api';
 import { cryptoWaitReady } from '@polkadot/util-crypto';
-import { u8aToHex } from '@polkadot/util';
+import { u8aToHex, assert } from '@polkadot/util';
 import { createSignedKeyUpdate } from '../src/utils/did';
 import { getPublicKeyFromKeyringPair } from '../src/utils/misc';
 import { DockAPI, PublicKeySr25519 } from '../src/api';
 import b58 from 'bs58';
 
-/// This script is not configurable via command line. You will need to maually edit `main()`
-/// to input your own values.
-///
+require('dotenv').config();
+
 /// Use (with node running locally):
 ///
 /// ```bash
 /// cd sdk
 /// yarn
-/// npx babel-node ./scripts/change_did_key.js
+/// npx babel-node ./scripts/change_did_key.js args..
 /// ```
+
+const USAGE = `\
+npx babel-node ./scripts/change_did_key.js <did> <new_pk>
+  where
+    <did> is a 32 byte, 0x prefixed hex string.
+    <new_pk> is a 32 byte, 0x prefixed hex string.
+
+env vars:
+  - FullNodeEndpoint: websocket Url of a trusted node
+  - DidHolderSecret:  current secret key of the did. provided as a substrate secret URI
+  - PayerSecret:      secret key to the on-chain account that will submit the transaction
+                      this account pays transaction fees. provided as a substrate secret URI
+
+Warning: This script only supports sr25519 keys. Do not use it to set an non-sr25519 public key.
+         If you do, you will lose control of your DID.
+`;
 
 (async () => {
   await main();
@@ -26,36 +46,31 @@ import b58 from 'bs58';
 });
 
 async function main() {
-  /// This did is preregistered for local development chains.
-  const ALICE_DID = '0x416c696365000000000000000000000000000000000000000000000000000000';
-  /// The default keypair for "Alice\0\0\0..." on local developnent chains.
-  const ALICE_KP = {
-    sk: '0x416c696365736b00000000000000000000000000000000000000000000000000',
-    pk: '0xb8b868f83227df83240fdbefcaa2636bebf16304c9249069897b510b7d8bbd0b',
-  };
-  /// Some arbitray sr25519 keypair.
-  const TEST_KP = {
-    sk: '0x6b9abb2153e0e3989bb53869b4a88806941a0a86cefa08a7977aff73379df734',
-    pk: '0xc081a33d27d66ceda4e9f634f2f3a1b3b7f4ad898662036416250862ffae623f',
-  };
+  const {
+    FullNodeEndpoint,
+    DidHolderSecret,
+    PayerSecret,
+  } = process.env;
+  assert(FullNodeEndpoint !== undefined, "env var FullNodeEndpoint must be defined");
+  assert(DidHolderSecret !== undefined, "env var DidHolderSecret must be defined");
+  assert(PayerSecret !== undefined, "env var PayerSecret must be defined");
 
-  // set key to TEST_KP
+  if (process.argv.length !== 4) {
+    throw USAGE;
+  }
+  const [_, __, did, new_pk] = process.argv;
+  assertHex32(did);
+  assertHex32(new_pk);
+
   await updateDIDKey({
-    nodeWsUrl: 'ws://localhost:9944',
-    did: ALICE_DID,
-    currentSk: ALICE_KP.sk,
-    newPk: TEST_KP.pk,
-    payerSk: "//Bob"
+    nodeWsUrl: FullNodeEndpoint,
+    did,
+    currentSk: DidHolderSecret,
+    newPk: new_pk,
+    payerSk: PayerSecret,
   });
 
-  // set it back to the default
-  await updateDIDKey({
-    nodeWsUrl: 'ws://localhost:9944',
-    did: ALICE_DID,
-    currentSk: TEST_KP.sk,
-    newPk: ALICE_KP.pk,
-    payerSk: "//Bob"
-  });
+  console.log(`Public key for did ${did} successfully updated to ${new_pk}.`)
 }
 
 // This function assumes all keys are sr25519.
@@ -85,9 +100,7 @@ async function updateDIDKey({ nodeWsUrl, did, currentSk, newPk, payerSk }) {
   const finalPk = await getSr25519PkHex(dock, did);
   assert_equal(finalPk, newPk, "paid transaction did not take effect, something went wrong");
 
-  // when this script was written there was a bug that would cause the sript to block forever when
-  // this line was uncommented.
-  // await dock.disconnect();
+  await dock.disconnect();
 }
 
 // Load a sr25519 keypair from secret, secret may be "0x" prefixed hex seed
@@ -110,4 +123,28 @@ async function getSr25519PkHex(dock, did) {
   const pk = doc.publicKey[0];
   assert_equal(pk.type, 'Sr25519VerificationKey2020', "this script assumes sr25519");
   return u8aToHex(b58.decode(pk.publicKeyBase58));
+}
+
+function assertHex32(v) {
+  if (!isHex32(v)) {
+    throw `"${v}" is not valid as a 32 byte lowecase 0x-prefixed hex-string`;
+  }
+}
+
+function isHex32(v) {
+  if ((typeof v) !== 'string') {
+    throw "bad type passed to isHex32";
+  }
+  if (!v.startsWith("0x")) {
+    return false;
+  }
+  for (let c of v.slice(2)) {
+    if (!"0123456789abcdef".includes(c)) {
+      return false;
+    }
+  }
+  if (v.length !== 66) {
+    return false;
+  }
+  return true;
 }

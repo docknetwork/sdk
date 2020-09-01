26a26824586163498deec3a47eab653a53d2aae6
Andrew Dirksen <andrew@dirksen.com>
2020-09-01 12:03:51 -0700

    heed eslint

--- example/create_master_proposal.js
+++ example/create_master_proposal.js
@@ -1,26 +1,27 @@
 // Example script creates a json-encoded proposal for master to vote on.
 
-import { connect } from '../scripts/helpers';
 import { u8aToHex, hexToU8a } from '@polkadot/util';
 import { randomAsU8a } from '@polkadot/util-crypto';
+import { connect } from '../scripts/helpers';
 
-main().catch(e => {
+require('dotenv').config();
+
+main().catch((e) => {
   console.error(e);
   process.exit(1);
-}).then(_ => {
+}).then(() => {
   process.exit(0);
 });
 
 async function main() {
-  require('dotenv').config();
   const { FullNodeEndpoint } = process.env;
 
   const nc = await connect(FullNodeEndpoint);
 
-  let proposal = nc.tx.system.setStorage([
-    [u8aToHex(randomAsU8a(32)), u8aToHex(randomAsU8a(32))]
+  const proposal = nc.tx.system.setStorage([
+    [u8aToHex(randomAsU8a(32)), u8aToHex(randomAsU8a(32))],
   ]);
-  let jprop = proposal.method.toJSON();
+  const jprop = proposal.method.toJSON();
 
   // Bug in polkadot-js makes hex-encoded call index unparsable so we convert to an array.
   jprop.callIndex = [...hexToU8a(jprop.callIndex)];

5d7f6aa300b37ac144b42c6fb9ff980fd377604c
Andrew Dirksen <andrew@dirksen.com>
2020-09-23 13:36:13 -0700

    implement conflation-free claimgraph merge

--- src/utils/claimgraph.js
+++ src/utils/claimgraph.js
@@ -1,13 +1,13 @@
 // A claimgraph is expressed as list of rdf triples.
 // A triple contains three Nodes, subject, property, and object.
 // A Node may be either:
-// an IRI: `{ "Iri": "<string>" }`,
-// a blank node: `{ "Blank": "<string>" }`,
-// or a blank node: `{ "Literal": { "value": "<string>", "type": <Node> } }`.
+// an IRI: `{ Iri: '<iri>' }`,
+// a blank node: `{ Blank: '<string>' }`,
+// or a literal node: `{ Literal: { value: '<string>', datatype: '<iri>' } }`
 //
-// This module implementes utilities for operating on claimgraphs if the above format.
+// This module implements utilities for operating on claimgraphs if the above format.
 
-export const claims = "https://www.dock.io/rdf2020#claimsV1";
+export const claims = { Iri: 'https://www.dock.io/rdf2020#claimsV1' };
 
 // Convert from the json-ld claimgraph representaion
 // https://github.com/digitalbazaar/jsonld.js/blob/983cd849f56180e2ee4552ca25dd52b293174830/lib/toRdf.js
@@ -21,22 +21,29 @@ export function fromJsonldjsCg(jcg) {
 // convert a single node from json-ld claimgraph representaion to this module's representation.
 function fromJsonldjsNode(jn) {
   switch (jn.termType) {
-    case "NamedNode":
+    case 'NamedNode':
       return { Iri: jn.value };
-    case "BlankNode":
+    case 'BlankNode':
       return { Blank: jn.value };
-    case "Literal":
-      return { Literal: { value: jn.value, type: fromJsonldjsNode(jn.datatype) } };
+    case 'Literal':
+      assert(jn.datatype.termType === 'NamedNode', 'The datatype of an RDF literal must be an IRI');
+      return { Literal: { value: jn.value, datatype: jn.datatype.value } };
     default:
-      throw "Unknown node type: " + jn.termType;
+      throw 'Unknown node type: ' + jn.termType;
   }
 }
 
-// Merge two claimgraphs without conflating blank node names.
+// Merge multiple claimgraphs without conflating blank node names.
 // When merging, blank nodes are renamed as required in order to avoid conflicts.
-export function merge(cga, cgb) {
-  todo();
-  return [];
+// Input is a list of claimgraphs.
+export function merge(claimgraphs) {
+  claimgraphs.forEach(assertValidClaimGraph);
+  let cgs = deepClone(claimgraphs);
+  let namer = new Namer();
+  for (let cg of cgs) {
+    namer.reallocateNames(cg);
+  }
+  return cgs.flat(1);
 }
 
 // Convert a Node into a cononicalized string representation.
@@ -48,14 +55,14 @@ export function canon(node) {
 
 // recursively lexically sort the keys in an object
 // expect(JSON.stringify(orderKeys(
-//   { "b": "", "a": "" }
+//   { b: '', a: '' }
 // ))).toEqual(JSON.stringify(
-//   { "a": "", "b": "" }
+//   { a: '', b: '' }
 // ));
 // expect(JSON.stringify(orderKeys(
-//   { "b": "", "a": { "c": "", "b": "", "a": "" } }
+//   { b: '', a: { c: '', b: '', a: '' } }
 // ))).toEqual(JSON.stringify(
-//   { "a": { "a": "", "b": "", "c": "" }, "b": "" }
+//   { a: { a: '', b: '', c: '' }, b: '' }
 // ));
 function orderKeys(a) {
   switch (typeof a) {
@@ -70,46 +77,130 @@ function orderKeys(a) {
       }
       return ret;
     default:
-      throw "type error: " + typeof a;
+      throw 'type error: orderKeys() does not accept type ' + typeof a;
   }
 }
 
 // Convert to Explicit Ethos form.
 // See https://www.w3.org/TR/WD-rdf-syntax-971002/ , 2.2. Utility Relations; "Layer 1", reification
-export function asEE(cg, issuerIRI) {
-  let usedBlanks = new Set();
-  for (let claim of cg) {
-    for (let node of claim) {
-      if (node.Blank !== undefined) {
-        usedBlanks.add(node.Blank);
-      }
-    }
-  }
-  let nextBlank = 0;
+export function asEE(claimgraph, issuerIRI) {
+  let cg = deepClone(claimgraph);
+  let namer = new Namer();
+  namer.reallocateNames(cg);
   let ret = [];
   for (let [s, p, o] of cg) {
-    while (usedBlanks.has(blank(nextBlank))) {
-      nextBlank++;
-    }
-    let statment = { Blank: blank(nextBlank) };
-    ret.push([{ Iri: issuerIRI }, { Iri: claims }, statment]);
-    ret.push([statment, { Iri: rdf('subject') }, s]);
-    ret.push([statment, { Iri: rdf('predicate') }, p]);
-    ret.push([statment, { Iri: rdf('object') }, o]);
-    nextBlank++;
+    let statement = namer.next();
+    ret.push([{ Iri: issuerIRI }, claims, statement]);
+    ret.push([statement, rdf('subject'), s]);
+    ret.push([statement, rdf('predicate'), p]);
+    ret.push([statement, rdf('object'), o]);
   }
   return ret;
 }
 
-function blank(n) {
-  return `_:b${n}`;
-}
-
 // It's common to use shorthand like `rdf:type`. This function let's us do something similar.
-// expect(rdf('type')).toBe('http://www.w3.org/1999/02/22-rdf-syntax-ns#type');
+// expect(rdf('type')).toEqual({ Iri: 'http://www.w3.org/1999/02/22-rdf-syntax-ns#type' });
 function rdf(keyword) {
   if (!['type', 'subject', 'predicate', 'object'].includes(keyword)) {
     throw `are you sure ${keyword} is part of the rdf: context?`;
   }
-  return 'http://www.w3.org/1999/02/22-rdf-syntax-ns#' + keyword;
+  return { Iri: 'http://www.w3.org/1999/02/22-rdf-syntax-ns#' + keyword };
+}
+
+// return the Set() of all bank node names in a claimgraph
+function allBlanks(cg) {
+  let ret = new Set();
+  for (let claim of cg) {
+    for (let node of claim) {
+      if (node.Blank !== undefined) {
+        ret.add(node.Blank);
+      }
+    }
+  }
+  return ret;
+}
+
+// Blank node name generator
+// An object of this type won't produce the same name twice, even accross multiple calls to
+// reallocateNames.
+class Namer {
+  constructor() {
+    this.nextBlank = 0;
+  }
+
+  // get the next available blank node
+  next() {
+    let ret = { Blank: `_:b${this.nextBlank}` };
+    this.nextBlank++;
+    return ret;
+  }
+
+  // modify claimgraph replacing blank nodes with names allocated by this namer
+  reallocateNames(claimgraph) {
+    const blanks = allBlanks(claimgraph);
+    let renames = {};
+    for (let name of blanks) {
+      renames[name] = this.next();
+    }
+    for (let claim of claimgraph) {
+      for (let i = 0; i < claim.length; i++) {
+        if (claim[i].Blank !== undefined) {
+          claim[i] = renames[claim[i].Blank];
+        }
+      }
+    }
+  }
+}
+
+// deep copy a json serializable object
+function deepClone(obj) {
+  // https://jsben.ch/E55IQ
+  return JSON.parse(JSON.stringify(obj));
+}
+
+function assertValidClaimGraph(cg) {
+  for (let claim of cg) {
+    for (let node of claim) {
+      assertValidNode(node);
+    }
+  }
+}
+
+function assertValidNode(node) {
+  let ks = Object.keys(node);
+  assert(ks.length === 1, 'enum representaion invalid. enum must be exactly one variant');
+  let tag = ks[0];
+  let value = node[tag];
+  switch (tag) {
+    case 'Iri':
+      assertType(value, 'string');
+      break;
+    case 'Blank':
+      assertType(value, 'string');
+      break;
+    case 'Literal':
+      assertType(value, 'object');
+      assert(Object.keys(value).length == 2);
+      assertType(value.value, 'string');
+      assertType(value.datatype, 'string');
+      break;
+    default:
+      throw 'Bad node tag: ' + tag;
+  }
+}
+
+function assertType(a, typ) {
+  if (typeof a !== typ) {
+    throw `Type error. Expected type ${typ}. Got ${a} which has type ${typeof a}.`;
+  }
+}
+
+function assert(b, s) {
+  if (!b) {
+    let m = 'assertion failed';
+    if (s !== undefined) {
+      m += ' ' + s;
+    }
+    throw m;
+  }
 }
--- tests/unit/claimgraph.test.js
+++ tests/unit/claimgraph.test.js
@@ -9,7 +9,7 @@ describe('Claimgraph operations.', () => {
         {
           Literal: {
             value: 'Example University',
-            type: { Iri: 'http://www.w3.org/1999/02/22-rdf-syntax-ns#HTML' }
+            datatype: 'http://www.w3.org/1999/02/22-rdf-syntax-ns#HTML'
           }
         },
         { Blank: '_:b1' }
@@ -27,8 +27,65 @@ describe('Claimgraph operations.', () => {
         { Blank: '_:b5' }
       ]
     ];
-    expect(merge(cg1, cg1)).toEqual([]);
-    expect(merge(cg1, cg2)).toEqual([]);
+    expect(merge([])).toEqual([]);
+    expect(merge([cg1])).toEqual([
+      [
+        { Iri: 'https://example.com/credentials/1872' },
+        {
+          Literal: {
+            value: 'Example University',
+            datatype: 'http://www.w3.org/1999/02/22-rdf-syntax-ns#HTML'
+          }
+        },
+        { Blank: '_:b0' }
+      ],
+      [
+        { Blank: '_:b0' },
+        { Blank: '_:b1' },
+        { Blank: '_:b2' }
+      ]
+    ]);
+    expect(merge([cg2])).toEqual([
+      [
+        { Blank: '_:b0' },
+        { Blank: '_:b1' },
+        { Blank: '_:b2' }
+      ]
+    ]);
+    expect(merge([cg2, cg2])).toEqual([
+      [
+        { Blank: '_:b0' },
+        { Blank: '_:b1' },
+        { Blank: '_:b2' }
+      ],
+      [
+        { Blank: '_:b3' },
+        { Blank: '_:b4' },
+        { Blank: '_:b5' }
+      ]
+    ]);
+    expect(merge([cg1, cg2])).toEqual([
+      [
+        { Iri: 'https://example.com/credentials/1872' },
+        {
+          Literal: {
+            value: 'Example University',
+            datatype: 'http://www.w3.org/1999/02/22-rdf-syntax-ns#HTML'
+          }
+        },
+        { Blank: '_:b0' }
+      ],
+      [
+        { Blank: '_:b0' },
+        { Blank: '_:b1' },
+        { Blank: '_:b2' }
+      ],
+      [
+        { Blank: '_:b3' },
+        { Blank: '_:b4' },
+        { Blank: '_:b5' }
+      ]
+    ]);
   });
 
   test('canon: ∀ A, B ∈ Node: canon(A) = canon(B) <-> A = B', async () => {
@@ -38,12 +95,12 @@ describe('Claimgraph operations.', () => {
       {
         Literal: {
           value: 'Example University',
-          type: { Iri: 'http://www.w3.org/1999/02/22-rdf-syntax-ns#HTML' }
+          datatype: 'http://www.w3.org/1999/02/22-rdf-syntax-ns#HTML'
         }
       },
       {
         Literal: { // the keys in this are swapped but the canonical representation shouldn't change
-          type: { Iri: 'http://www.w3.org/1999/02/22-rdf-syntax-ns#HTML' },
+          datatype: 'http://www.w3.org/1999/02/22-rdf-syntax-ns#HTML',
           value: 'Example University'
         }
       },
@@ -66,7 +123,7 @@ describe('Claimgraph operations.', () => {
         {
           Literal: {
             value: 'Example University',
-            type: { Iri: 'http://www.w3.org/1999/02/22-rdf-syntax-ns#HTML' }
+            datatype: 'http://www.w3.org/1999/02/22-rdf-syntax-ns#HTML'
           }
         }
       ], [
@@ -78,51 +135,49 @@ describe('Claimgraph operations.', () => {
     let eecg = asEE(cg, 'did:dock:bobert');
     expect(eecg).toEqual([
       [
-        { "Iri": "did:dock:bobert" },
-        { "Iri": "https://www.dock.io/rdf2020#claimsV1" },
-        { "Blank": "_:b0" }
+        { Iri: 'did:dock:bobert' },
+        { Iri: 'https://www.dock.io/rdf2020#claimsV1' },
+        { Blank: '_:b1' }
       ],
       [
-        { "Blank": "_:b0" },
-        { "Iri": "http://www.w3.org/1999/02/22-rdf-syntax-ns#subject" },
-        { "Iri": "did:example:ebfeb1f712ebc6f1c276e12ec21" }
+        { Blank: '_:b1' },
+        { Iri: 'http://www.w3.org/1999/02/22-rdf-syntax-ns#subject' },
+        { Iri: 'did:example:ebfeb1f712ebc6f1c276e12ec21' }
       ],
       [
-        { "Blank": "_:b0" },
-        { "Iri": "http://www.w3.org/1999/02/22-rdf-syntax-ns#predicate" },
-        { "Iri": "http://schema.org/alumniOf" }
+        { Blank: '_:b1' },
+        { Iri: 'http://www.w3.org/1999/02/22-rdf-syntax-ns#predicate' },
+        { Iri: 'http://schema.org/alumniOf' }
       ],
       [
-        { "Blank": "_:b0" },
-        { "Iri": "http://www.w3.org/1999/02/22-rdf-syntax-ns#object" },
+        { Blank: '_:b1' },
+        { Iri: 'http://www.w3.org/1999/02/22-rdf-syntax-ns#object' },
         {
-          "Literal": {
-            "value": "Example University",
-            "type": {
-              "Iri": "http://www.w3.org/1999/02/22-rdf-syntax-ns#HTML"
-            }
+          Literal: {
+            value: 'Example University',
+            datatype: 'http://www.w3.org/1999/02/22-rdf-syntax-ns#HTML'
           }
         }
       ],
       [
-        { "Iri": "did:dock:bobert" },
-        { "Iri": "https://www.dock.io/rdf2020#claimsV1" },
-        { "Blank": "_:b2" }
+        { Iri: 'did:dock:bobert' },
+        { Iri: 'https://www.dock.io/rdf2020#claimsV1' },
+        { Blank: '_:b2' }
       ],
       [
-        { "Blank": "_:b2" },
-        { "Iri": "http://www.w3.org/1999/02/22-rdf-syntax-ns#subject" },
-        { "Iri": "https://example.com/credentials/1872" }
+        { Blank: '_:b2' },
+        { Iri: 'http://www.w3.org/1999/02/22-rdf-syntax-ns#subject' },
+        { Iri: 'https://example.com/credentials/1872' }
       ],
       [
-        { "Blank": "_:b2" },
-        { "Iri": "http://www.w3.org/1999/02/22-rdf-syntax-ns#predicate" },
-        { "Iri": "https://w3id.org/security#proof" }
+        { Blank: '_:b2' },
+        { Iri: 'http://www.w3.org/1999/02/22-rdf-syntax-ns#predicate' },
+        { Iri: 'https://w3id.org/security#proof' }
       ],
       [
-        { "Blank": "_:b2" },
-        { "Iri": "http://www.w3.org/1999/02/22-rdf-syntax-ns#object" },
-        { "Blank": "_:b1" }
+        { Blank: '_:b2' },
+        { Iri: 'http://www.w3.org/1999/02/22-rdf-syntax-ns#object' },
+        { Blank: '_:b0' }
       ]
     ]);
   });
@@ -130,23 +185,23 @@ describe('Claimgraph operations.', () => {
   test('convert claimgraph from jsonld-js represention', async () => {
     let jcg = [
       {
-        "subject": { "termType": "NamedNode", "value": "did:example:ebfeb1f712ebc6f1c276e12ec21" },
-        "predicate": { "termType": "NamedNode", "value": "http://schema.org/alumniOf" },
-        "object": {
-          "termType": "Literal",
-          "value": "Example University",
-          "datatype": {
-            "termType": "NamedNode",
-            "value": "http://www.w3.org/1999/02/22-rdf-syntax-ns#HTML"
+        subject: { termType: 'NamedNode', value: 'did:example:ebfeb1f712ebc6f1c276e12ec21' },
+        predicate: { termType: 'NamedNode', value: 'http://schema.org/alumniOf' },
+        object: {
+          termType: 'Literal',
+          value: 'Example University',
+          datatype: {
+            termType: 'NamedNode',
+            value: 'http://www.w3.org/1999/02/22-rdf-syntax-ns#HTML'
           }
         },
-        "graph": { "termType": "BlankNode", "value": "_:b0" }
+        graph: { termType: 'BlankNode', value: '_:b0' }
       },
       {
-        "subject": { "termType": "NamedNode", "value": "https://example.com/credentials/1872" },
-        "predicate": { "termType": "NamedNode", "value": "https://w3id.org/security#proof" },
-        "object": { "termType": "BlankNode", "value": "_:b1" },
-        "graph": { "termType": "BlankNode", "value": "_:b0" }
+        subject: { termType: 'NamedNode', value: 'https://example.com/credentials/1872' },
+        predicate: { termType: 'NamedNode', value: 'https://w3id.org/security#proof' },
+        object: { termType: 'BlankNode', value: '_:b1' },
+        graph: { termType: 'BlankNode', value: '_:b0' }
       }
     ];
     expect(fromJsonldjsCg(jcg)).toEqual([
@@ -156,7 +211,7 @@ describe('Claimgraph operations.', () => {
         {
           Literal: {
             value: 'Example University',
-            type: { Iri: 'http://www.w3.org/1999/02/22-rdf-syntax-ns#HTML' }
+            datatype: 'http://www.w3.org/1999/02/22-rdf-syntax-ns#HTML'
           }
         }
       ], [

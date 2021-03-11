f2eff9293b989f4f34ab82087db51de2d6c136ec
Sam Hellawell <sshellawell@gmail.com>
2021-03-11 23:26:51 +0000

    Turtle -> RIFY parser

---
+++ src/utils/rdf.js
@@ -0,0 +1,62 @@
+import { Parser } from 'n3';
+
+const rdfTermTypeMap = {
+  BlankNode: 'Blank',
+  NamedNode: 'Iri',
+};
+
+export function formatRDFTerm(type, rdf) {
+  const rdfName = rdfTermTypeMap[type] || type;
+  const formattedRDF = {};
+  if (rdfName === 'Literal') {
+    const { value, datatype } = rdf.toJSON();
+    formattedRDF[rdfName] = {
+      value,
+      datatype: datatype.value,
+    };
+  } else {
+    formattedRDF[rdfName] = rdf.value;
+  }
+  return formattedRDF;
+}
+
+export function parseRDFDocument(document, parserOptions = {}) {
+  const parser = new Parser(parserOptions);
+
+  // Parse document using N3, should return a list of quads
+  const parsedResult = parser.parse(document);
+
+  // Determine if we should format as quads or triples
+  const useQuads = parsedResult.map((quad) => quad.graph.termType === 'DefaultGraph' && !quad.value).indexOf(true) === -1;
+
+  // Format document to format SDK expects
+  return parsedResult.map((quad) => {
+    const {
+      subject,
+      predicate,
+      object,
+      graph,
+    } = quad;
+
+    // Format subject, predicate and object terms into rify standard
+    const formattedSubject = formatRDFTerm(subject.termType, subject);
+    const formattedPredicate = formatRDFTerm(predicate.termType, predicate);
+    const formattedObject = formatRDFTerm(object.termType, object);
+
+    // Format result as triple
+    const result = [
+      formattedSubject,
+      formattedPredicate,
+      formattedObject,
+    ];
+
+    // Add graph to complete quad type
+    if (useQuads) {
+      // TODO: formatted graph if should be quad
+      const formattedGraph = formatRDFTerm(graph.termType, graph);
+      result.push(formattedGraph);
+    }
+
+    return result;
+  });
+}
---
+++ tests/unit/rdf-turtle.test.js
@@ -0,0 +1,138 @@
+import { randomAsU8a } from '@polkadot/util-crypto';
+import { u8aToHex } from '@polkadot/util';
+
+import { parseRDFDocument } from '../../src/utils/rdf';
+
+const rdfInputs = [
+  `@prefix : <http://example.org/stuff/1.0/> .
+  :a :b ( "apple" "banana" ) .`,
+  `@prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> .
+  @prefix dc: <http://purl.org/dc/elements/1.1/> .
+  @prefix ex: <http://example.org/stuff/1.0/> .
+  <http://www.w3.org/TR/rdf-syntax-grammar>
+    dc:title "RDF/XML Syntax Specification (Revised)" ;
+    ex:editor [
+      ex:fullname "Dave Beckett";
+      ex:homePage <http://purl.org/net/dajobe/>
+    ] .`,
+  `<http://one.example/subject1> <http://one.example/predicate1> <http://one.example/object1> <http://example.org/graph3> . # comments here
+# or on a line by themselves
+_:subject1 <http://an.example/predicate1> "object1" <http://example.org/graph1> .
+_:subject2 <http://an.example/predicate2> "object2" <http://example.org/graph5> .`
+];
+
+describe('RDF Turtle Parsing', () => {
+  test('Can parse and format RDF test 1 (triples)', async () => {
+    const result = parseRDFDocument(rdfInputs[0]);
+    const expectedResult = [
+      [
+        { Blank: expect.anything() },
+        { Iri: "http://www.w3.org/1999/02/22-rdf-syntax-ns#first" },
+        { Literal: {
+          value: "apple",
+          datatype: "http://www.w3.org/2001/XMLSchema#string",
+        } },
+      ],
+      [
+        { Blank: expect.anything() },
+        { Iri: "http://www.w3.org/1999/02/22-rdf-syntax-ns#rest" },
+        { Blank: expect.anything() },
+      ],
+      [
+        { Blank: expect.anything() },
+        { Iri: "http://www.w3.org/1999/02/22-rdf-syntax-ns#first" },
+        { Literal: {
+          value: "banana",
+          datatype: "http://www.w3.org/2001/XMLSchema#string",
+        } },
+      ],
+      [
+        { Blank: expect.anything() },
+        { Iri: "http://www.w3.org/1999/02/22-rdf-syntax-ns#rest" },
+        { Iri: "http://www.w3.org/1999/02/22-rdf-syntax-ns#nil" },
+      ],
+      [
+        { Iri: "http://example.org/stuff/1.0/a" },
+        { Iri: "http://example.org/stuff/1.0/b" },
+        { Blank: expect.anything() },
+      ],
+    ];
+
+    expect(result.length).toEqual(expectedResult.length);
+    expect(result).toEqual(expect.arrayContaining(expectedResult));
+  });
+
+  test('Can parse and format RDF test 2 (triples)', async () => {
+    const result = parseRDFDocument(rdfInputs[1]);
+    const expectedResult = [
+      [
+        { Iri: "http://www.w3.org/TR/rdf-syntax-grammar" },
+        { Iri: "http://purl.org/dc/elements/1.1/title" },
+        { Literal: {
+          value: "RDF/XML Syntax Specification (Revised)",
+          datatype: "http://www.w3.org/2001/XMLSchema#string",
+        } },
+      ],
+      [
+        { Iri: "http://www.w3.org/TR/rdf-syntax-grammar" },
+        { Iri: "http://example.org/stuff/1.0/editor" },
+        { Blank: expect.anything() },
+      ],
+      [
+        { Blank: expect.anything() },
+        { Iri: "http://example.org/stuff/1.0/fullname" },
+        { Literal: {
+          value: "Dave Beckett",
+          datatype: "http://www.w3.org/2001/XMLSchema#string",
+        } },
+      ],
+      [
+        { Blank: expect.anything() },
+        { Iri: "http://example.org/stuff/1.0/homePage" },
+        { Iri: "http://purl.org/net/dajobe/" },
+      ],
+    ];
+
+    expect(result.length).toEqual(expectedResult.length);
+    expect(result).toEqual(expect.arrayContaining(expectedResult));
+  });
+
+  test('Can parse and format RDF test 3 (quads)', async () => {
+    const result = parseRDFDocument(rdfInputs[2]);
+    const expectedResult = [
+      [
+        { Iri: 'http://one.example/subject1' },
+        { Iri: 'http://one.example/predicate1' },
+        { Iri: 'http://one.example/object1' },
+        { Iri: "http://example.org/graph3" }
+      ],
+      [
+        { Blank: expect.anything() },
+        { Iri: 'http://an.example/predicate1' },
+        { Literal: {
+            "value": "object1",
+            "datatype": "http://www.w3.org/2001/XMLSchema#string"
+          },
+        },
+        { Iri: "http://example.org/graph1" }
+      ],
+      [
+        { Blank: expect.anything() },
+        { Iri: 'http://an.example/predicate2' },
+        { Literal: {
+            "value": "object2",
+            "datatype": "http://www.w3.org/2001/XMLSchema#string"
+          },
+        },
+        { Iri: "http://example.org/graph5" }
+      ]
+    ];
+
+    expect(result.length).toEqual(expectedResult.length);
+    expect(result).toEqual(expect.arrayContaining(expectedResult));
+  });
+
+  // test('Can fetch RDF document from IPFS', async () => {
+  //   expect(true).toBe(false);
+  // });
+});

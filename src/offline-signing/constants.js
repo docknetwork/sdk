386c1aa2746a85629a177185debf12356a5674b4
lovesh <lovesh.bond@gmail.com>
2021-05-24 21:25:20 +0530

    Upgrade dependencies
    Signed-off-by: lovesh <lovesh.bond@gmail.com>

--- src/offline-signing/constants.js
+++ src/offline-signing/constants.js
@@ -14,7 +14,7 @@ export const MAINNET_INFO = {
   name: 'Dock Mainnet',
   specName: 'dock-main-runtime',
   // Next 2 fields need to change whenever they change on the chain.
-  specVersion: 21,
+  specVersion: 24,
   transactionVersion: 1,
 };
 
@@ -25,11 +25,11 @@ export const TESTNET_INFO = {
     tokenDecimals: 6,
     tokenSymbol: 'DCK',
   },
-  genesis: '0x3f0608444cf5d7eec977430483ffef31ff86dfa6bfc6d7114023ee80cc03ea3f',
+  genesis: '0xd3ed57c8ce8744c0c581c7c3ea168553aa0fd208b2fc3c6c25d24f7f344d8b4a',
   name: 'Poa Testnet',
   specName: 'dock-testnet',
   // Next 2 fields need to change whenever they change on the chain.
-  specVersion: 20,
+  specVersion: 24,
   transactionVersion: 1,
 };
 
@@ -40,10 +40,11 @@ export const DEVNODE_INFO = {
     tokenDecimals: 6,
     tokenSymbol: 'DCK',
   },
+  // This should change whenever dev node changes
   genesis: '0x956cd95ad4baf709e851e0fce555bffa5ae2933851cadfaeeb13fe68fa74dfc5',
   name: 'Dock Mainnet',
   specName: 'dock-main-runtime',
   // Next 2 fields need to change whenever they change on the chain.
-  specVersion: 21,
+  specVersion: 24,
   transactionVersion: 1,
 };

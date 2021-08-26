1ea0f17d84db8c5b2378051a2efe262aff4ee16e
Samuel Hellawell <sshellawell@gmail.com>
2021-08-26 19:13:10 +0100

    move schemas, remove bol validation as url fails now

--- example/standard_schemas.js
+++ example/standard_schemas.js
@@ -3,50 +3,12 @@ import Schema from '../src/modules/schema';
 import { expandJSONLD } from '../src/utils/vc/helpers';
 import { validateCredentialSchema } from '../src/utils/vc/schema';
 
-import bolSchema from '../src/utils/vc/schemas/bol';
-import prCardSchema from '../src/utils/vc/schemas/pr_card';
-import qpInbonSchema from '../src/utils/vc/schemas/qp_inbond';
-import healthWorkerPassportSchema from '../src/utils/vc/schemas/health_worker_passport';
-import infectionDiagnosisSchema from '../src/utils/vc/schemas/infection_diagnosis';
-import immunityEventRecordSchema from '../src/utils/vc/schemas/immunity_event_record';
-import noInfectionSchema from '../src/utils/vc/schemas/non_infection_check';
-
-// Schema from here https://github.com/w3c-ccg/vc-examples/blob/master/plugfest-2020/vendors/mavennet/credentials/BillOfLading.json
-const bolCred = {
-  '@context': [
-    'https://www.w3.org/2018/credentials/v1',
-    'https://schema.org/',
-    'https://mavennet.github.io/contexts/bill-of-lading-v1.0.jsonld',
-  ],
-  id: 'http://neo-flow.com/credentials/e94a16cb-35b2-4301-9fb6-7af3d8fe7b81',
-  type: ['VerifiableCredential', 'BillOfLadingCredential'],
-  name: 'Bill of Lading',
-  description: 'Detailed shipment document provided by the carrier to the receiver of products.',
-  issuer: 'did:v1:test:nym:z6MkfG5HTrBXzsAP8AbayNpG3ZaoyM4PCqNPrdWQRSpHDV6J',
-  issuanceDate: '2020-04-09T21:13:13Z',
-  credentialSubject: {
-    productIdentifier: '3a185b8f-078a-4646-8343-76a45c2856a5',
-    bolNumber: 'BOL000104976',
-    carrier: 'did:v1:test:nym:z6MkhdmzFu659ZJ4XKj31vtEDmjvsi5yDZG5L7Caz63oP39k',
-    recipient: 'did:v1:test:nym:z6MknQLHcwKwopce5ji1Ftsurn8mNL58wTxZB238uEMsegUj',
-    transportType: 'Pipeline',
-    originAddress: {
-      address: 'Quebec, CAN',
-      latitude: 52.9399,
-      longitude: 73.5491,
-    },
-    deliveryAddress: {
-      address: 'Chicago, USA',
-      latitude: 41.8781,
-      longitude: 87.6298,
-    },
-    valuePerItem: 46,
-    totalOrderValue: 126500,
-    freightChargeTerms: 'Freight Prepaid',
-    expectedDeliveryDates: '2020-04-12',
-    comments: '',
-  },
-};
+import prCardSchema from './schemas/pr_card';
+import qpInbonSchema from './schemas/qp_inbond';
+import healthWorkerPassportSchema from './schemas/health_worker_passport';
+import infectionDiagnosisSchema from './schemas/infection_diagnosis';
+import immunityEventRecordSchema from './schemas/immunity_event_record';
+import noInfectionSchema from './schemas/non_infection_check';
 
 // Schema from here https://github.com/w3c-ccg/vc-examples/blob/master/plugfest-2020/vendors/sicpa/credentials/PermanentResidentCard.json
 const credPRCard = {
@@ -262,7 +224,6 @@ async function validateSchema(schema, credential) {
 }
 
 async function main() {
-  await validateSchema(bolSchema, bolCred);
   await validateSchema(prCardSchema, credPRCard);
   await validateSchema(qpInbonSchema, qPInbondCred);
   await validateSchema(healthWorkerPassportSchema, healthCareWorkerCred);

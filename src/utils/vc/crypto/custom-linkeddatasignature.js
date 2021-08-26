89612e788eeaeb518b7b2d5d84521fa98fcbc9f9
Samuel Hellawell <sshellawell@gmail.com>
2021-08-26 19:07:03 +0100

    fix import

--- src/utils/vc/crypto/custom-linkeddatasignature.js
+++ src/utils/vc/crypto/custom-linkeddatasignature.js
@@ -1,5 +1,5 @@
 import { suites } from 'jsonld-signatures';
-import base58btc from 'base58-universal';
+import { encode, decode } from 'base58-universal';
 import base64url from 'base64url';
 
 const MULTIBASE_BASE58BTC_HEADER = 'z';
@@ -51,7 +51,7 @@ export default class CustomLinkedDataSignature extends suites.LinkedDataSignatur
       if (proofValue[0] !== MULTIBASE_BASE58BTC_HEADER) {
         throw new Error('Only base58btc multibase encoding is supported.');
       }
-      signatureBytes = base58btc.decode(proofValue.substr(1));
+      signatureBytes = decode(proofValue.substr(1));
     } else if (jws && typeof jws === 'string') { // Fallback to older jsonld-signature implementations
       const [encodedHeader, /* payload */, encodedSignature] = jws.split('.');
 
@@ -107,7 +107,7 @@ export default class CustomLinkedDataSignature extends suites.LinkedDataSignatur
     const signatureBytes = await this.signer.sign({ data: verifyData });
     return {
       ...proof,
-      proofValue: MULTIBASE_BASE58BTC_HEADER + base58btc.encode(signatureBytes),
+      proofValue: MULTIBASE_BASE58BTC_HEADER + encode(signatureBytes),
     };
   }
 }

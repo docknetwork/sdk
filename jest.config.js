602634eea6526a79d5d8d1cb58d7f2703b245ee7
Samuel Hellawell <sshellawell@gmail.com>
2022-07-11 18:42:26 +0100

    jest adjustments
    Signed-off-by: Samuel Hellawell <sshellawell@gmail.com>

--- jest.config.js
+++ jest.config.js
@@ -1,15 +1,8 @@
 module.exports = {
   clearMocks: true,
   testTimeout: 30000,
-  // testEnvironment: './tests/test-environment.js',
-  transform: {
-    '^.+\\.(ts|js)$': 'babel-jest',
-  },
   transformIgnorePatterns: [
     '/node_modules/(?!@polkadot|@babel)',
-  ],
-  setupFilesAfterEnv: [
-    './tests/setup-test-env.js',
   ],
   globals: {
     Uint8Array,

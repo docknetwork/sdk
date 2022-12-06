c260714c809d73c8bd15995870b4772d26d991ee
Osinakayah Ifeanyi <ifeanyiosinakayah15@gmail.com>
2022-12-06 19:48:22 +0100

    Test

--- tests/integration/anoncreds/bbs-plus-presentation.test.js
+++ tests/integration/anoncreds/bbs-plus-presentation.test.js
@@ -1,6 +1,8 @@
 import { randomAsHex } from '@polkadot/util-crypto';
 import { hexToU8a, u8aToHex, stringToHex } from '@polkadot/util';
-import { initializeWasm, KeypairG2, SignatureParamsG1 } from '@docknetwork/crypto-wasm-ts';
+import { initializeWasm, BBSPlusSecretKey, BBSPlusPublicKeyG2, SignatureParamsG1 } from '@docknetwork/crypto-wasm-ts';
+import { CredentialSchema, CredentialBuilder, PresentationBuilder } from '@docknetwork/crypto-wasm-ts/lib/anonymous-credentials';
+import b58 from 'bs58';
 import { DockAPI } from '../../../src';
 import { FullNodeEndpoint, TestAccountURI, TestKeyringOpts } from '../../test-constants';
 import { createNewDockDID } from '../../../src/utils/did';
@@ -40,8 +42,75 @@ const residentCardSchema = {
   },
 };
 
+const residentCardSchema1 = {
+  $schema: 'http://json-schema.org/draft-07/schema#',
+  $id: 'https://ld.dock.io/examples/resident-card-schema.json',
+  title: 'Resident Card Example',
+  type: 'object',
+  properties: {
+    '@context': {
+      title: 'Context',
+      type: 'array',
+      items: [{type: 'string'}, {type: 'string'}, {type: 'string'}],
+    },
+    id: {
+      title: 'Id',
+      type: 'string',
+    },
+    type: {
+      title: 'Type',
+      type: 'array',
+      items: [{type: 'string'}, {type: 'string'}],
+    },
+    identifier: {
+      title: 'identifier',
+      type: 'string',
+    },
+    name: {
+      title: 'Name',
+      type: 'string',
+    },
+    description: {
+      title: 'Desc',
+      type: 'string',
+    },
+    credentialSubject: {
+      type: 'object',
+      properties: {
+        id: {
+          title: 'Id',
+          type: 'string',
+        },
+        type: {
+          title: 'Type',
+          type: 'array',
+          items: [{type: 'string'}, {type: 'string'}],
+        },
+        givenName: {
+          title: 'Given Name',
+          type: 'string',
+        },
+        familyName: {
+          title: 'Family Name',
+          type: 'string',
+        },
+        lprNumber: {
+          title: 'LPR Number',
+          type: 'integer',
+          minimum: 0,
+        },
+      },
+    },
+  },
+};
+
 const embeddedSchema = {
   id: `data:application/json;charset=utf-8,${encodeURIComponent(JSON.stringify(residentCardSchema))}`,
+  type: 'JsonSchemaValidator2018',
+};
+
+const embeddedSchema1 = {
+  id: `data:application/json;charset=utf-8,${encodeURIComponent(JSON.stringify(residentCardSchema1))}`,
   type: 'JsonSchemaValidator2018',
 };
 
@@ -66,6 +135,31 @@ const credentialJSON = {
     familyName: 'SMITH',
     lprNumber: 1234,
   },
+};
+
+const credentialJSONWithoutSchema = {
+  '@context': [
+    'https://www.w3.org/2018/credentials/v1',
+    'https://w3id.org/citizenship/v1',
+    'https://ld.dock.io/security/bbs/v1',
+  ],
+  id: 'https://issuer.oidp.uscis.gov/credentials/83627465',
+  type: ['VerifiableCredential', 'PermanentResidentCard'],
+  identifier: '83627465',
+  name: 'Permanent Resident Card',
+  description: 'Government of Example Permanent Resident Card.',
+  credentialSubject: {
+    id: 'did:example:b34ca6cd37bbf23',
+    type: ['PermanentResident', 'Person'],
+    givenName: 'JOHN',
+    familyName: 'SMITH',
+    lprNumber: 1234,
+  },
+};
+
+const credentialJSON1 = {
+  ...credentialJSONWithoutSchema,
+  credentialSchema: embeddedSchema1,
 };
 
 describe('BBS+ Module', () => {
@@ -106,6 +200,31 @@ describe('BBS+ Module', () => {
     expect(publicKey[1].type).toEqual('Bls12381G2VerificationKeyDock2022');
 
     keypair.id = publicKey[1].id;
+  }, 30000);
+
+  test('Can create a credential and presentation', async () => {
+    const didDocument = await dock.did.getDocument(did1);
+    const pkRaw = b58.decode(didDocument.publicKey[1].publicKeyBase58);
+    const pk = new BBSPlusPublicKeyG2(pkRaw);
+
+    const credSchema = new CredentialSchema(residentCardSchema1, { useDefaults: true });
+    const credBuilder = new CredentialBuilder();
+    credBuilder.schema = credSchema;
+    credBuilder.subject = credentialJSONWithoutSchema.credentialSubject;
+
+    for (const k of ['@context', 'id', 'type', 'identifier', 'name', 'description']) {
+      credBuilder.setTopLevelField(k, credentialJSONWithoutSchema[k]);
+    }
+
+    const sk = new BBSPlusSecretKey(keypair.privateKeyBuffer);
+    const credential = credBuilder.sign(sk, undefined, {requireAllFieldsFromSchema: false});
+    expect(credential.verify(pk).verified).toEqual(true);
+    // console.log(credential.toJSON());
+
+    const presBuilder = new PresentationBuilder();
+    presBuilder.addCredential(credential, pk);
+    const pres = presBuilder.finalize();
+    expect(pres.verify([pk]).verified).toEqual(true);
   }, 30000);
 
   test('Can issue+verify a BBS+ credential with embedded schema', async () => {

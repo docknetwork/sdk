cd8b8614b739e5ea2383991f27c72fddc53584cc
Osinakayah Ifeanyi <ifeanyiosinakayah15@gmail.com>
2022-12-06 20:07:47 +0100

    Test

--- tests/integration/anoncreds/bbs-plus-presentation.test.js
+++ tests/integration/anoncreds/bbs-plus-presentation.test.js
@@ -1,46 +1,16 @@
 import { randomAsHex } from '@polkadot/util-crypto';
-import { hexToU8a, u8aToHex, stringToHex } from '@polkadot/util';
-import { initializeWasm, BBSPlusSecretKey, BBSPlusPublicKeyG2, SignatureParamsG1 } from '@docknetwork/crypto-wasm-ts';
+import { u8aToHex } from '@polkadot/util';
+import { initializeWasm, BBSPlusSecretKey, BBSPlusPublicKeyG2 } from '@docknetwork/crypto-wasm-ts';
 import { CredentialSchema, CredentialBuilder, PresentationBuilder } from '@docknetwork/crypto-wasm-ts/lib/anonymous-credentials';
 import b58 from 'bs58';
 import { DockAPI } from '../../../src';
 import { FullNodeEndpoint, TestAccountURI, TestKeyringOpts } from '../../test-constants';
 import { createNewDockDID } from '../../../src/utils/did';
 import Bls12381G2KeyPairDock2022 from '../../../src/utils/vc/crypto/Bls12381G2KeyPairDock2022';
+import BbsPlusPresentation from '../../../src/bbs-plus-presentation';
 
 import BBSPlusModule from '../../../src/modules/bbs-plus';
-import { DockResolver } from '../../../src/resolver';
-import { registerNewDIDUsingPair, getCredMatcherDoc, getProofMatcherDoc } from '../helpers';
-import { issueCredential, verifyCredential } from '../../../src/utils/vc/index';
-import getKeyDoc from '../../../src/utils/vc/helpers';
-
-const residentCardSchema = {
-  $schema: 'http://json-schema.org/draft-07/schema#',
-  $id: 'https://ld.dock.io/examples/resident-card-schema.json',
-  title: 'Resident Card Example',
-  type: 'object',
-  properties: {
-    credentialSubject: {
-      type: 'object',
-      properties: {
-        givenName: {
-          title: 'Given Name',
-          type: 'string',
-        },
-        familyName: {
-          title: 'Family Name',
-          type: 'string',
-        },
-        lprNumber: {
-          title: 'LPR Number',
-          type: 'integer',
-          minimum: 0,
-        },
-      },
-      required: [],
-    },
-  },
-};
+import { registerNewDIDUsingPair } from '../helpers';
 
 const residentCardSchema1 = {
   $schema: 'http://json-schema.org/draft-07/schema#',
@@ -104,38 +74,6 @@ const residentCardSchema1 = {
   },
 };
 
-const embeddedSchema = {
-  id: `data:application/json;charset=utf-8,${encodeURIComponent(JSON.stringify(residentCardSchema))}`,
-  type: 'JsonSchemaValidator2018',
-};
-
-const embeddedSchema1 = {
-  id: `data:application/json;charset=utf-8,${encodeURIComponent(JSON.stringify(residentCardSchema1))}`,
-  type: 'JsonSchemaValidator2018',
-};
-
-const credentialJSON = {
-  '@context': [
-    'https://www.w3.org/2018/credentials/v1',
-    'https://w3id.org/citizenship/v1',
-    'https://ld.dock.io/security/bbs/v1',
-  ],
-  id: 'https://issuer.oidp.uscis.gov/credentials/83627465',
-  type: ['VerifiableCredential', 'PermanentResidentCard'],
-  credentialSchema: embeddedSchema,
-  identifier: '83627465',
-  name: 'Permanent Resident Card',
-  description: 'Government of Example Permanent Resident Card.',
-  issuanceDate: '2019-12-03T12:19:52Z',
-  expirationDate: '2029-12-03T12:19:52Z',
-  credentialSubject: {
-    id: 'did:example:b34ca6cd37bbf23',
-    type: ['PermanentResident', 'Person'],
-    givenName: 'JOHN',
-    familyName: 'SMITH',
-    lprNumber: 1234,
-  },
-};
 
 const credentialJSONWithoutSchema = {
   '@context': [
@@ -157,14 +95,11 @@ const credentialJSONWithoutSchema = {
   },
 };
 
-const credentialJSON1 = {
-  ...credentialJSONWithoutSchema,
-  credentialSchema: embeddedSchema1,
-};
 
-describe('BBS+ Module', () => {
+
+describe('BBS+ Presentation', () => {
   const dock = new DockAPI();
-  const resolver = new DockResolver(dock);
+  let bbsPlusPresentation;
   let account;
   let did1;
   let pair1;
@@ -183,6 +118,7 @@ describe('BBS+ Module', () => {
     pair1 = dock.keyring.addFromUri(randomAsHex(32));
     did1 = createNewDockDID();
     await registerNewDIDUsingPair(dock, did1, pair1);
+    bbsPlusPresentation = new BbsPlusPresentation(dock);
   }, 20000);
 
   test('Can create BBS+ public key for the DID', async () => {
@@ -217,7 +153,7 @@ describe('BBS+ Module', () => {
     }
 
     const sk = new BBSPlusSecretKey(keypair.privateKeyBuffer);
-    const credential = credBuilder.sign(sk, undefined, {requireAllFieldsFromSchema: false});
+    const credential = credBuilder.sign(sk, undefined, { requireAllFieldsFromSchema: false });
     expect(credential.verify(pk).verified).toEqual(true);
     // console.log(credential.toJSON());
 
@@ -226,60 +162,6 @@ describe('BBS+ Module', () => {
     const pres = presBuilder.finalize();
     expect(pres.verify([pk]).verified).toEqual(true);
   }, 30000);
-
-  test('Can issue+verify a BBS+ credential with embedded schema', async () => {
-    const issuerKey = getKeyDoc(did1, keypair, keypair.type, keypair.id);
-    const unsignedCred = {
-      ...credentialJSON,
-      issuer: did1,
-    };
-
-    const credential = await issueCredential(issuerKey, unsignedCred);
-    expect(credential).toMatchObject(
-      expect.objectContaining(
-        getCredMatcherDoc(unsignedCred, did1, issuerKey.id, 'Bls12381BBS+SignatureDock2022'),
-      ),
-    );
-
-    const result = await verifyCredential(credential, { resolver });
-    expect(result).toMatchObject(
-      expect.objectContaining(
-        getProofMatcherDoc(),
-      ),
-    );
-
-    // Ensure embedding the schema doesnt conflict with the other schema resolutions
-    const resultWithSchema = await verifyCredential(credential, { resolver, schemaApi: { dock }, });
-    expect(resultWithSchema).toMatchObject(
-      expect.objectContaining(
-        getProofMatcherDoc(),
-      ),
-    );
-  }, 30000);
-
-  test('Can issue+verify a BBS+ credential with default schema', async () => {
-    const issuerKey = getKeyDoc(did1, keypair, keypair.type, keypair.id);
-    const unsignedCred = {
-      ...credentialJSON,
-      issuer: did1,
-    };
-    delete unsignedCred.credentialSchema;
-
-    const credential = await issueCredential(issuerKey, unsignedCred);
-    expect(credential).toMatchObject(
-      expect.objectContaining(
-        getCredMatcherDoc(unsignedCred, did1, issuerKey.id, 'Bls12381BBS+SignatureDock2022'),
-      ),
-    );
-
-    const result = await verifyCredential(credential, { resolver });
-    expect(result).toMatchObject(
-      expect.objectContaining(
-        getProofMatcherDoc(),
-      ),
-    );
-  }, 30000);
-
   afterAll(async () => {
     await dock.disconnect();
   }, 10000);

9a415caad01c966b0b93a38ee67dceab8a1f819a
olegnn <olegnosov1@gmail.com>
2023-08-23 18:14:41 +0200

    Validator stash payout calculator

--- scripts/helpers.js
+++ scripts/helpers.js
@@ -8,6 +8,7 @@ import {
   always,
   ifElse,
   has,
+  map,
   prop,
   when,
   isNil,
@@ -18,11 +19,10 @@ import {
   curry,
   __,
   unless,
+  fromPairs,
 } from "ramda";
 import { Observable } from "rxjs";
-
-import dock from "../src";
-import { DockAPI } from '../src';
+import { DockAPI } from "../src";
 
 /**
  * Send the give transaction with the given account URI (secret) and return the block hash
@@ -124,7 +124,7 @@ export async function keypair(seed) {
  * @param wsUrl - string
  * @returns {Promise}
  */
- export async function connect(wsUrl) {
+export async function connect(wsUrl) {
   /*return await ApiPromise.create({
     provider: new WsProvider(wsUrl),
     typesBundle,
@@ -270,10 +270,16 @@ export const timeout = curry((time, promise) =>
  * Formats given value as DOCK token amount.
  *
  * @param {BN} value
+ * @param {?object} config
  * @returns {string}
  */
-export const formatDock = (value) =>
-  formatBalance(value, { decimals: 6, withSi: true, withUnit: "DCK" });
+export const formatDock = (value, config = {}) =>
+  formatBalance(value, {
+    decimals: 6,
+    withSi: true,
+    ...config,
+    withUnit: "DCK",
+  });
 
 /**
  * Enhances supplied function by providing access to the initialized global dock API.
@@ -289,15 +295,16 @@ export const formatDock = (value) =>
 export const withDockAPI = curry((params, fn) => async (...args) => {
   console.log("Connecting...");
   let err, res;
+  const dockAPI = new DockAPI();
 
   try {
-    await dock.init(params);
-    res = await fn(dock, ...args);
+    await dockAPI.init(params);
+    res = await fn(dockAPI, ...args);
   } catch (e) {
     err = e;
   } finally {
     console.log("Disconnecting...");
-    await dock.disconnect();
+    await dockAPI.disconnect();
 
     if (err) {
       throw err;
@@ -314,3 +321,132 @@ export const withDockAPI = curry((params, fn) => async (...args) => {
  * @returns {bool}
  */
 export const parseBool = either(equals("true"), o(Boolean, Number));
+
+/**
+ * Queries validator identity.
+ * @param {*} api
+ * @param {*} accountId
+ * @returns {Promise<string>}
+ */
+export const validatorIdentity = curry(async (api, accountId) => {
+  const val = api.createType(
+    "Option<Registration>",
+    await api.query.identity.identityOf(api.createType("AccountId", accountId))
+  );
+
+  if (val.isSome) {
+    const info = val.unwrap().info;
+
+    return `\`${api.createType(
+      "String",
+      info.toHuman().display.Raw
+    )}\` (${accountId.toString()})`;
+  } else {
+    return `\`${accountId.toString()}\``;
+  }
+});
+
+export const formatAsISO = (date) =>
+  date.toISOString().replace(/T/, " ").replace(/\..+/, "");
+
+export const addLoggerPrefix = curry((buildPrefix, logger) =>
+  o(
+    fromPairs,
+    map((key) => [key, (...args) => logger[key](buildPrefix(...args), ...args)])
+  )(["error", "log", "info", "warn"])
+);
+
+/**
+ * Returns hash and number of the first block satisfying provided predicate.
+ * Throws an error in case such block can't be found.
+ *
+ * @template T
+ * @param {*} api
+ * @param {{ start: number, end: number }}
+ * @param {{ targetValue: T, fetchValue: (number) => T, checkBlock: (block: any) => boolean }} endBlockNumber
+ * @param {{ maxBlocksPerUnit: number = null, debug = false }} targetEra
+ * @returns {T}
+ */
+export const binarySearchBlock = async (
+  api,
+  { start: startBlockNumber, end: endBlockNumber },
+  { targetValue, fetchValue, checkBlock },
+  { maxBlocksPerUnit = null, debug = false }
+) => {
+  // Number of iterations performed during binary search.
+  let jumps = 0;
+  while (startBlockNumber < endBlockNumber) {
+    jumps++;
+    const midBlockNumber = ((startBlockNumber + endBlockNumber) / 2) | 0;
+    const midBlockHash = await api.rpc.chain.getBlockHash(midBlockNumber);
+    const midValue = await fetchValue(midBlockHash);
+
+    if (debug)
+      timestampLogger.log(
+        "target value:",
+        targetValue,
+        "current value:",
+        midValue,
+        "start block:",
+        startBlockNumber,
+        "end block:",
+        endBlockNumber,
+        "jumps:",
+        jumps
+      );
+
+    if (midValue < targetValue) {
+      startBlockNumber = midBlockNumber + 1;
+      if (maxBlocksPerUnit != null) {
+        endBlockNumber = Math.min(
+          midBlockNumber + maxBlocksPerUnit * (1 + targetValue - midValue),
+          endBlockNumber
+        );
+      }
+    } else if (midValue > targetValue) {
+      endBlockNumber = midBlockNumber;
+      if (maxBlocksPerUnit != null) {
+        startBlockNumber = Math.max(
+          midBlockNumber - maxBlocksPerUnit * (1 + midValue - targetValue),
+          startBlockNumber
+        );
+      }
+    } else {
+      endBlockNumber = midBlockNumber;
+      if (maxBlocksPerUnit != null) {
+        startBlockNumber = Math.max(
+          midBlockNumber - maxBlocksPerUnit,
+          startBlockNumber
+        );
+      }
+
+      const midBlock = await api.derive.chain.getBlock(midBlockHash);
+      const found = checkBlock(midBlock);
+
+      if (found) {
+        if (debug)
+          timestampLogger.log(
+            `First block that satisfied value \`${targetValue}\` found - \`${midBlockNumber}\` (${jumps} jumps)`,
+            found
+          );
+
+        return { hash: midBlockHash, number: midBlockNumber };
+      }
+    }
+  }
+
+  throw new Error(`No block found`);
+};
+
+/**
+ * Overrides `log`, `error`, `info`, and `warn` methods of the console to put a timestamp at the beginning.
+ * @typedef TimestampLogger
+ * @prop {Function} log
+ * @prop {Function} error
+ * @prop {Function} info
+ * @prop {Function} warn
+ */
+export const timestampLogger = addLoggerPrefix(
+  () => `[${formatAsISO(new Date())}]`,
+  console
+);
---
+++ scripts/validator_stash_payouts.js
@@ -0,0 +1,383 @@
+import {
+  withDockAPI,
+  formatDock,
+  envObj,
+  notNilAnd,
+  finiteNumber,
+  parseBool,
+  timestampLogger,
+  binarySearchBlock,
+} from "./helpers";
+import { o, defaultTo, unless, either, curry, fromPairs, map } from "ramda";
+import BN from "bn.js";
+import { validateAddress } from "../src/utils/chain-ops";
+import {
+  toArray,
+  from,
+  mergeMap,
+  lastValueFrom,
+  switchMap,
+  map as mapRx,
+} from "rxjs";
+import { ApiPromise } from "@polkadot/api";
+
+const greaterThanZero = unless(
+  (value) => value > 0,
+  () => {
+    throw new Error("Must be greater than zero");
+  }
+);
+
+const {
+  FullNodeEndpoint,
+  Stash,
+  StartEra,
+  ErasCount,
+  IncludeBlocks,
+  Debug,
+  BlocksPerEra,
+  ConcurrentErasLimit,
+} = envObj({
+  // Address of the node RPC.
+  FullNodeEndpoint: notNilAnd(String),
+  // Account to summarise payouts for.
+  Stash: o(
+    unless(
+      either(
+        (addr) => validateAddress(addr, "test"),
+        (addr) => validateAddress(addr, "main")
+      ),
+      (addr) => {
+        throw new Error(`Invalid stash address: ${addr}`);
+      }
+    ),
+    notNilAnd(String)
+  ),
+  // Start era to summarise payouts from.
+  StartEra: o(greaterThanZero, notNilAnd(finiteNumber)),
+  // Amount of eras to calculate payout for starting from `StartEra`.
+  ErasCount: o(greaterThanZero, notNilAnd(finiteNumber)),
+  // Include block counts into the reports.
+  IncludeBlocks: parseBool,
+  // Enable debug logs.
+  Debug: parseBool,
+  // Number of blocks per a single era.
+  BlocksPerEra: o(finiteNumber, defaultTo(14400)),
+  // Number of eras to be processed concurrently
+  ConcurrentErasLimit: o(finiteNumber, defaultTo(10)),
+});
+
+const main = withDockAPI(
+  {
+    address: FullNodeEndpoint,
+  },
+  async (dock) => {
+    const currentEraIndex = (await dock.api.query.staking.activeEra())
+      .unwrap()
+      .index.toNumber();
+
+    if (currentEraIndex < StartEra + ErasCount) {
+      throw new Error(
+        `\`StartEra\` + \`ErasCount\` must be less or equal to ${currentEraIndex}`
+      );
+    }
+
+    const eraIndices = Array.from(
+      { length: ErasCount },
+      (_, idx) => StartEra + idx
+    );
+    const lastBlock = (await dock.api.query.system.number()).toNumber();
+
+    const payouts$ = from(eraIndices).pipe(
+      mergeMap(
+        o(from, async (eraIndex) => {
+          const startBlock = Math.max(
+            lastBlock - BlocksPerEra * (currentEraIndex - eraIndex),
+            0
+          );
+
+          const { hash: eraPaidBlockHash, number } = await findEraPaidBlock(
+            dock,
+            startBlock,
+            lastBlock,
+            eraIndex
+          );
+
+          const eraPaidApi = await dock.api.at(eraPaidBlockHash);
+
+          const blocks = IncludeBlocks
+            ? (await fetchProducedBlockCounts(dock, number)).join(", ")
+            : null;
+
+          const eraValidatorInfo = await fetchValidatorStashEraInfo(
+            eraPaidApi,
+            new BN(eraIndex),
+            Stash
+          );
+
+          let validatorPoints = new BN(0);
+          for (const [key, value] of eraValidatorInfo.points.individual) {
+            if (key.toString() === Stash) {
+              validatorPoints = value;
+            }
+          }
+
+          const eraValidatorPayout = validatorPoints.isZero()
+            ? {
+                total: new BN(0),
+                staking: new BN(0),
+                commission: new BN(0),
+              }
+            : validatorStashPayout(
+                eraValidatorInfo.prefs,
+                validatorPoints,
+                eraValidatorInfo.exposure,
+                eraValidatorInfo.points.total,
+                eraValidatorInfo.rewards
+              );
+
+          return [
+            eraIndex,
+            { ...eraValidatorPayout, blocks, prefs: eraValidatorInfo.prefs },
+          ];
+        }),
+        ConcurrentErasLimit
+      )
+    );
+    const payouts = await lastValueFrom(payouts$.pipe(toArray()));
+
+    const { total, staking, commission } = payouts
+      .sort(([i1], [i2]) => i1 - i2)
+      .reduce(
+        (acc, [index, { total, staking, commission, blocks, prefs }]) => {
+          timestampLogger.log(
+            `Era ${index}: paid = \`${formatDock(
+              total
+            )}\` (staking = ${formatDock(staking)}, commission = ${formatDock(
+              commission
+            )}), commission = ${prefs.commission.toNumber() / 1e7}%${
+              blocks ? `, blocks produced = ${blocks}` : ""
+            }`
+          );
+
+          return {
+            total: acc.total.add(total),
+            staking: acc.staking.add(staking),
+            commission: acc.commission.add(commission),
+            blocks: acc.blocks + blocks,
+          };
+        },
+        {
+          total: new BN(0),
+          staking: new BN(0),
+          commission: new BN(0),
+          blocks: 0,
+        }
+      );
+
+    timestampLogger.log(
+      `Summarised stash payout for ${Stash} in ${StartEra}-${
+        StartEra + ErasCount - 1
+      } eras - total = \`${formatDock(total, {
+        forceUnit: "DCK",
+      })}\`: staking = \`${formatDock(staking, {
+        forceUnit: "DCK",
+      })}\`, commission = \`${formatDock(commission, { forceUnit: "DCK" })}\``
+    );
+  }
+);
+
+const fetchProducedBlockCounts = async (dock, number) => {
+  const api = await apiAtBlock(dock, number - 1);
+  const lastSessionIndex = await api.query.session.currentIndex();
+
+  const sessionEnds = await Promise.all(
+    Array.from({ length: 3 }, (_, idx) =>
+      findNewSession(
+        dock,
+        number - (BlocksPerEra * (idx + 1)) / 4,
+        number,
+        lastSessionIndex.toNumber() - idx - 1
+      )
+    ).reverse()
+  );
+
+  const sessionAPIs = [
+    ...(await Promise.all(
+      sessionEnds.map(({ number }) => apiAtBlock(dock, number - 1))
+    )),
+    api,
+  ];
+
+  return await Promise.all(
+    sessionAPIs.map((api) => lastValueFrom(countStashSessionBlocks(api, Stash)))
+  );
+};
+
+/**
+ * Instantiates an API at the block with the supplied number.
+ * @param {*} dock
+ * @param {number} number
+ * @returns {ApiPromise}
+ */
+const apiAtBlock = async (dock, number) =>
+  await dock.api.at(await dock.api.rpc.chain.getBlockHash(number));
+
+/**
+ * Returns the amount of blocks produced by the supplied stash during the current session.
+ * @param {*} api
+ * @param {*} stash
+ * @returns {Observable<number>}
+ */
+const countStashSessionBlocks = curry((api, stash) =>
+  from(api.query.session.currentIndex()).pipe(
+    switchMap((sessionIndex) =>
+      from(api.query.imOnline.authoredBlocks(sessionIndex, stash))
+    ),
+    mapRx((authoredBlocks) => authoredBlocks.toNumber())
+  )
+);
+
+/**
+ * Returns hash of the block when `EraPaid` was emitted for the `targetEra`.
+ * Throws an error in case such block can't be found.
+ *
+ * @param {*} dock
+ * @param {*} startBlockNumber
+ * @param {*} endBlockNumber
+ * @param {*} targetEra
+ */
+const findEraPaidBlock = async (
+  dock,
+  startBlockNumber,
+  endBlockNumber,
+  targetEra
+) => {
+  return binarySearchBlock(
+    dock.api,
+    { start: startBlockNumber, end: endBlockNumber },
+    {
+      fetchValue: async (blockHash) => {
+        const activeEra = await dock.api.query.staking.activeEra.at(blockHash);
+
+        return activeEra.unwrap().index.toNumber();
+      },
+      targetValue: targetEra + 1,
+      checkBlock: (block) =>
+        block.events
+          .toHuman()
+          .find(
+            ({ event: { method, section } }) =>
+              section === "staking" &&
+              (method === "EraPayout" || method === "EraPaid")
+          ),
+    },
+    { maxBlocksPerUnit: BlocksPerEra, debug: Debug }
+  );
+};
+
+/**
+ * Returns hash of the block when `NewSession` was emitted for the `targetSession`.
+ * Throws an error in case such block can't be found.
+ *
+ * @param {*} dock
+ * @param {*} startBlockNumber
+ * @param {*} endBlockNumber
+ * @param {*} targetEra
+ */
+const findNewSession = async (
+  dock,
+  startBlockNumber,
+  endBlockNumber,
+  targetSession
+) => {
+  return binarySearchBlock(
+    dock.api,
+    { start: startBlockNumber, end: endBlockNumber },
+    {
+      fetchValue: async (blockHash) => {
+        const sessionIndex = await dock.api.query.session.currentIndex.at(
+          blockHash
+        );
+
+        return sessionIndex.toNumber();
+      },
+      targetValue: targetSession + 1,
+      checkBlock: (block) =>
+        block.events
+          .toHuman()
+          .find(
+            ({ event: { method, section } }) =>
+              section === "session" && method === "NewSession"
+          ),
+    },
+    { maxBlocksPerUnit: (BlocksPerEra / 4) | 0, debug: Debug }
+  );
+};
+/**
+ * Fetches information about the era for the supplied validator.
+ *
+ * @param dock
+ * @param blockHash
+ * @param eraIndex
+ * @param stash
+ */
+const fetchValidatorStashEraInfo = async (api, eraIndex, stash) => {
+  const [rewards, points, prefs, exposure] = await Promise.all([
+    api.query.staking.erasValidatorReward(eraIndex),
+    api.query.staking.erasRewardPoints(eraIndex),
+    api.query.staking.erasValidatorPrefs(eraIndex, stash),
+    api.query.staking.erasStakersClipped(eraIndex, stash),
+  ]);
+
+  return {
+    points,
+    rewards: rewards.unwrap(),
+    prefs,
+    exposure,
+  };
+};
+
+/**
+ * Calculates `total`, `staking` and `commission` payout for the validator stash.
+ *
+ * @param validatorPref
+ * @param validatorRewardPoints
+ * @param validatorExposure
+ * @param eraRewardPoints
+ * @param eraPayout
+ */
+const validatorStashPayout = (
+  validatorPref,
+  validatorRewardPoints,
+  validatorExposure,
+  eraRewardPoints,
+  eraPayout
+) => {
+  // This is how much validator + nominators are entitled to.
+  const validatorTotalPayout = eraPayout
+    .mul(validatorRewardPoints)
+    .div(eraRewardPoints);
+
+  // Validator first gets a cut off the top.
+  const validatorCommissionPayout = new BN(
+    validatorTotalPayout.toNumber() *
+      (validatorPref.commission.toNumber() / 1e9)
+  );
+  const validatorLeftOverPart = validatorTotalPayout.sub(
+    validatorCommissionPayout
+  );
+
+  // Now let's calculate how this is split to the validator.
+  const validatorStakingPayout = validatorLeftOverPart
+    .mul(new BN(validatorExposure.own.toNumber()))
+    .div(new BN(validatorExposure.total.toNumber()));
+
+  return {
+    total: validatorStakingPayout.add(validatorCommissionPayout),
+    staking: validatorStakingPayout,
+    commission: validatorCommissionPayout,
+  };
+};
+
+main().catch(timestampLogger.error);

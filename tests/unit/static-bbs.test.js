9c3582bd31e5b2518f4ea16f4bc9208aab4cd5d6
Sam Hellawell <sshellawell@gmail.com>
2023-09-25 19:39:16 +0100

    Static BBS+ cred test
    Signed-off-by: Sam Hellawell <sshellawell@gmail.com>

---
+++ tests/unit/static-bbs.test.js
@@ -0,0 +1,26 @@
+import { verifyCredential } from '../../src/utils/vc/index';
+import defaultDocumentLoader from '../../src/utils/vc/document-loader';
+import didDocument from '../data/static-did-dock.json';
+import staticCred610 from '../data/static-bbs-cred-610.json';
+
+const loadDocumentDefault = defaultDocumentLoader(null);
+
+describe('Testing isHexWithGivenByteSize', () => {
+  test('verifies BBS+ credential from SDK 6.1.0', async () => {
+    const result = await verifyCredential(staticCred610, {
+      documentLoader: async (doc) => {
+        if (doc.startsWith(staticCred610.issuer.id)) {
+          return {
+            contextUrl: null,
+            documentUrl: doc,
+            document: didDocument,
+          };
+        }
+
+        return loadDocumentDefault(doc);
+      },
+    });
+    console.error(JSON.stringify(result, null, 2));
+    expect(result.verified).toBe(true);
+  });
+});

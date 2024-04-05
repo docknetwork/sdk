fd8149eea09b123d21618c159e813853b238ca9c
Oleg Nosov <olegnosov1@gmail.com>
2024-04-05 17:20:18 +0900

    Doc tweaks

--- src/utils/generic.js
+++ src/utils/generic.js
@@ -16,8 +16,21 @@ export class MapWithCapacity extends Map {
     this.adjustSize();
   }
 
+  /**
+   * Associates supplied value with the provided key.
+   * If the capacity was reached, earliest item added to this map will be removed.
+   * Unlike with the standard `Map`, the latest set item will always be emitted
+   * last by the iterables, even in case it was already added and then overriden.
+   *
+   * @param key
+   * @param value
+   */
   set(key, value) {
+    // `Map` iterables produce items in the insertion order.
+    // Thus we have to remove a possibly existing item from the map to make the new entry emitted
+    // last by the iterators produced by calling `entries`/`keys`/`values` methods.
     this.delete(key);
+
     const res = super.set(key, value);
     this.adjustSize();
 

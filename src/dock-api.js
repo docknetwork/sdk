572316d394dab3e2eebbdda1a7695da2ce968349
Sam Hellawell <sshellawell@gmail.com>
2024-04-22 15:53:54 +0100

    typo fix

--- src/dock-api.js
+++ src/dock-api.js
@@ -108,7 +108,7 @@ export default class DockAPI {
     const isWebsocket = addressStr && addressStr.indexOf('http') === -1;
 
     if (!isWebsocket && addressArray.length > 1) {
-      console.warn('WARNINGL HTTP connections do not support more than one URL, ignoring rest');
+      console.warn('WARNING: HTTP connections do not support more than one URL, ignoring rest');
     }
 
     const provider = isWebsocket

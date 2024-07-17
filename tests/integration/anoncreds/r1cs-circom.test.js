41a69db8d8d65672f32399fe40d88aec3774a183
lovesh <lovesh.bond@gmail.com>
2024-07-17 23:25:21 +0530

    Fix encoding
    Signed-off-by: lovesh <lovesh.bond@gmail.com>

--- tests/integration/anoncreds/r1cs-circom.test.js
+++ tests/integration/anoncreds/r1cs-circom.test.js
@@ -132,7 +132,7 @@ for (const {
       await initializeWasm();
 
       // Setup encoder
-      const defaultEncoder = (v) => Signature.encodeMessageForSigning(
+      const defaultEncoder = (v) => Signature.encodeMessageForSigningConstantTime(
         Uint8Array.from(Buffer.from(v.toString(), 'utf-8')),
       );
       encoder = new Encoder(undefined, defaultEncoder);

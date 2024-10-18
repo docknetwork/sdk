f7c4ddd6d9d73e9c10815d64a681854fe3dfc52e
olegnn <olegnosov1@gmail.com>
2024-10-18 19:39:24 +0200

    `await`

--- packages/dock-blockchain-modules/src/trust-registry/module.js
+++ packages/dock-blockchain-modules/src/trust-registry/module.js
@@ -45,23 +45,26 @@ export default class DockTrustRegistryModule extends injectDock(
   async createRegistryTx(id, info, schemas, didKeypair) {
     return await this.dockOnly.apiProvider.batchAll(
       await Promise.all(
-        this.dockOnly.apiProvider.withDidNonce(didKeypair.did, (nonce) => [
-          this.dockOnly.initOrUpdateTx(
-            info.convener,
-            id,
-            info.name,
-            info.govFramework,
-            didKeypair,
-            nonce.inc(),
-          ),
-          this.dockOnly.setSchemasMetadataTx(
-            didKeypair.did,
-            id,
-            { Set: schemas },
-            didKeypair,
-            nonce.inc(),
-          ),
-        ]),
+        await this.dockOnly.apiProvider.withDidNonce(
+          didKeypair.did,
+          (nonce) => [
+            this.dockOnly.initOrUpdateTx(
+              info.convener,
+              id,
+              info.name,
+              info.govFramework,
+              didKeypair,
+              nonce.inc(),
+            ),
+            this.dockOnly.setSchemasMetadataTx(
+              didKeypair.did,
+              id,
+              { Set: schemas },
+              didKeypair,
+              nonce.inc(),
+            ),
+          ],
+        ),
       ),
     );
   }

b4d5b15c17d7f3a7d44989b95a7e45152facfcc6
olegnn <olegnosov1@gmail.com>
2024-10-31 18:11:17 +0100

    Lints

--- packages/credential-sdk/src/resolver/status-list2021/status-list2021-resolver.js
+++ packages/credential-sdk/src/resolver/status-list2021/status-list2021-resolver.js
@@ -1,17 +1,17 @@
-import { ensureInstanceOf } from "../../utils";
-import AbstractStatusListCredentialModule from "../../modules/abstract/status-list-credential/module";
+import { ensureInstanceOf } from '../../utils';
+import AbstractStatusListCredentialModule from '../../modules/abstract/status-list-credential/module';
 import {
   HEX_ID_REG_EXP_PATTERN,
   METHOD_REG_EXP_PATTERN,
   Resolver,
-} from "../generic";
+} from '../generic';
 
 const STATUS_LIST_ID_MATCHER = new RegExp(
-  `^status-list2021:${METHOD_REG_EXP_PATTERN}:${HEX_ID_REG_EXP_PATTERN}$`
+  `^status-list2021:${METHOD_REG_EXP_PATTERN}:${HEX_ID_REG_EXP_PATTERN}$`,
 );
 
 class StatusListResolver extends Resolver {
-  prefix = "status-list2021";
+  prefix = 'status-list2021';
 
   get method() {
     return this.statusListCredentialModule.methods();
@@ -29,7 +29,7 @@ class StatusListResolver extends Resolver {
      */
     this.statusListCredentialModule = ensureInstanceOf(
       statusListCredentialModule,
-      AbstractStatusListCredentialModule
+      AbstractStatusListCredentialModule,
     );
   }
 
@@ -37,13 +37,13 @@ class StatusListResolver extends Resolver {
     const match = statusListId.match(STATUS_LIST_ID_MATCHER);
     if (!match) {
       throw new Error(
-        `Invalid \`StatusList2021Credential\` id: \`${statusListId}\``
+        `Invalid \`StatusList2021Credential\` id: \`${statusListId}\``,
       );
     }
     const [, _, id] = statusListId.match(STATUS_LIST_ID_MATCHER);
 
     const cred = await this.statusListCredentialModule.getStatusListCredential(
-      id
+      id,
     );
 
     return cred?.value.list.toJSON();

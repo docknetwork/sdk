156f1561e069eb2e2797110747c556600007107e
olegnn <olegnosov1@gmail.com>
2024-12-06 13:22:31 +0400

    Fix `toJSON`/`fromJSON` for `cheqd` payloads

--- packages/cheqd-blockchain-modules/src/common/payload.js
+++ packages/cheqd-blockchain-modules/src/common/payload.js
@@ -1,9 +1,9 @@
 import {
   VerificationMethodSignature,
   CheqdDid,
+  CheqdDIDDocument,
 } from '@docknetwork/credential-sdk/types';
 import {
-  Any,
   TypedArray,
   TypedString,
   TypedStruct,
@@ -11,21 +11,37 @@ import {
   TypedUUID,
 } from '@docknetwork/credential-sdk/types/generic';
 
-export class CheqdPayloadAndSignatures extends TypedStruct {
-  static Classes = {
-    payload: Any,
-    signatures: class Signatures extends TypedArray {
-      static Class = VerificationMethodSignature;
+const createTypes = (Payload) => {
+  const payloadWithSigsName = `CheqdPayloadWithSignatures(${Payload.name})`;
+  const payloadWithTypeUrlAndSigsName = `CheqdPayloadWithTypeUrlAndSignatures(${Payload.name})`;
+
+  const obj1 = {
+    [payloadWithSigsName]: class extends TypedStruct {
+      static Classes = {
+        payload: Payload,
+        signatures: class Signatures extends TypedArray {
+          static Class = VerificationMethodSignature;
+        },
+      };
     },
   };
-}
-
-export class CheqdPayloadWithTypeUrl extends TypedStruct {
-  static Classes = {
-    typeUrl: class TypeURL extends TypedString {},
-    value: CheqdPayloadAndSignatures,
+  const obj2 = {
+    [payloadWithTypeUrlAndSigsName]: class extends TypedStruct {
+      static Classes = {
+        typeUrl: class TypeURL extends TypedString {},
+        value: obj1[payloadWithSigsName],
+      };
+    },
   };
-}
+
+  // eslint-disable-next-line
+  Payload.ResourcePayloadWithTypeUrlAndSignatures = obj2[payloadWithTypeUrlAndSigsName];
+
+  return [
+    obj1[payloadWithSigsName],
+    obj2[payloadWithTypeUrlAndSigsName],
+  ];
+};
 
 export class CheqdCreateResource extends TypedStruct {
   static Classes = {
@@ -40,3 +56,25 @@ export class CheqdCreateResource extends TypedStruct {
     data: class Data extends TypedBytesArray {},
   };
 }
+
+export class DeactivateDidDocument extends TypedStruct {
+  static Classes = {
+    id: CheqdDid,
+    versionId: TypedUUID,
+  };
+}
+
+export const [
+  CheqdCreateResourcePayloadWithSignatures,
+  CheqdCreateResourcePayloadWithTypeUrlAndSignatures,
+] = createTypes(CheqdCreateResource);
+
+export const [
+  CheqdSetDidDocumentPayloadWithSignatures,
+  CheqdSetDidDocumentPayloadWithTypeUrlAndSignatures,
+] = createTypes(CheqdDIDDocument);
+
+export const [
+  CheqdDeactivateDidDocumentPayloadWithSignatures,
+  CheqdDeactivateDidDocumentPayloadWithTypeUrlAndSignatures,
+] = createTypes(DeactivateDidDocument);
--- packages/cheqd-blockchain-modules/src/did/internal.js
+++ packages/cheqd-blockchain-modules/src/did/internal.js
@@ -1,19 +1,11 @@
 import { CheqdDid } from '@docknetwork/credential-sdk/types/did/onchain/typed-did';
 import {
   TypedUUID,
-  TypedStruct,
 } from '@docknetwork/credential-sdk/types/generic';
 import { DIDDocument } from '@docknetwork/credential-sdk/types/did';
-import { createInternalCheqdModule } from '../common';
+import { createInternalCheqdModule, DeactivateDidDocument } from '../common';
 
 const parseDocument = (document) => DIDDocument.from(document).toCheqd();
-
-class DeactivateDidDocument extends TypedStruct {
-  static Classes = {
-    id: CheqdDid,
-    versionId: TypedUUID,
-  };
-}
 
 const methods = {
   createDidDocument: parseDocument,

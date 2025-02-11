e38b405edc445c0978e895d187c2ab02707f2d3f
Oleg <olegnosov1@gmail.com>
2025-02-11 21:40:46 +0800

    Increase timeout

--- packages/credential-sdk/src/modules/tests/common.js
+++ packages/credential-sdk/src/modules/tests/common.js
@@ -2,5 +2,5 @@
 export const testIf =
   (filter) =>
   (...args) =>
-    filter(...args) ? test(...args) : test.skip(...args);
+    filter(...args) ? test(...args, 6e4) : test.skip(...args);
 /* eslint-enable */

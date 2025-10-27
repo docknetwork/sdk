69635ffafb550f2d8755e07d05ac1b457f35ad92
Sam <3194321+cykoder@users.noreply.github.com>
2025-10-27 15:51:46 +0000

    Credential delegation/redelegation tests (#548)
    * Credential delegation/redelegation tests
    
    * unhappy path test cases
    
    * rename method

---
+++ packages/credential-sdk/tests/utils/did-helpers.js
@@ -0,0 +1,72 @@
+import b58 from 'bs58';
+import { randomAsHex } from '../../src/utils';
+import { Secp256k1Keypair } from '../../src/keypairs';
+import { verifyCredential, verifyPresentation } from '../../src/vc';
+import { documentLoader, addDocument, registered } from './cached-document-loader';
+
+export function registerDid(did, keypair) {
+  if (registered(did)) {
+    throw `${did} already registered`;
+  }
+
+  const publicKeyBase58 = b58.encode(keypair.publicKey.value.bytes);
+  const pk = {
+    id: `${did}#keys-1`,
+    type: keypair.type,
+    publicKeyBase58,
+    controller: did,
+    publicKey: keypair.publicKey.value,
+  };
+  const doc = {
+    '@context': 'https://www.w3.org/ns/did/v1',
+    id: did,
+    authentication: [pk.id],
+    assertionMethod: [pk.id],
+    publicKey: [pk],
+  };
+  addDocument(did, doc);
+  addDocument(pk.id, {
+    '@context': 'https://www.w3.org/ns/did/v1',
+    ...pk,
+    pkdoc: true,
+  });
+}
+
+export function genPlaceholderDID() {
+  return `did:dock:${randomAsHex(32).substring(2, 2 + 20)}`;
+}
+
+export async function verifyC(credential) {
+  return verifyCredential(credential, {
+    documentLoader,
+  });
+}
+
+export async function verifyP(presentation) {
+  return verifyPresentation(presentation, {
+    documentLoader,
+    unsignedPresentation: true,
+  });
+}
+
+export function getSampleKey(randomDID, keypair) {
+  return {
+    id: `${randomDID}#keys-1`,
+    controller: randomDID,
+    type: 'EcdsaSecp256k1VerificationKey2019',
+    keypair,
+    thisisstring: 'yes',
+    publicKey: keypair.publicKey(),
+  };
+}
+
+export async function newDid() {
+  const kp = Secp256k1Keypair.random();
+  const randomDID = genPlaceholderDID();
+  const keypair = getSampleKey(randomDID, kp);
+  registerDid(randomDID, keypair);
+  return {
+    did: randomDID,
+    suite: keypair,
+  };
+}

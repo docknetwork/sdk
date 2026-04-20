c85c882dbe4beb04f249c762fc1fd169c79b1aee
Sam Hellawell <sshellawell@gmail.com>
2026-04-20 21:12:13 +0100

    Allow to resolve status list from http/https too

--- packages/credential-sdk/src/resolver/status-list2021/status-list2021-resolver.js
+++ packages/credential-sdk/src/resolver/status-list2021/status-list2021-resolver.js
@@ -1,6 +1,14 @@
 import { ensureInstanceOf, ensureString } from '../../utils';
 import AbstractStatusListCredentialModule from '../../modules/abstract/status-list-credential/module';
 import { Resolver } from '../generic';
+import jsonFetch from '../../utils/json-fetch';
+import { StatusList2021Credential } from '../../vc';
+
+const HTTP_PREFIXES = ['http://', 'https://'];
+
+function isHttpUrl(id) {
+  return HTTP_PREFIXES.some((prefix) => id.startsWith(prefix));
+}
 
 class StatusListResolver extends Resolver {
   prefix = 'status-list2021';
@@ -25,15 +33,32 @@ class StatusListResolver extends Resolver {
     );
   }
 
-  async resolve(id) {
-    const cred = await this.statusListCredentialModule.getStatusListCredential(ensureString(id));
+  supports(id) {
+    const identifier = ensureString(id);
 
-    return cred?.toJSON();
+    return isHttpUrl(identifier) || super.supports(identifier);
+  }
+
+  async resolve(id) {
+    const identifier = ensureString(id);
+    if (isHttpUrl(identifier)) {
+      const doc = await jsonFetch(identifier);
+
+      try {
+        return StatusList2021Credential.fromJSON(doc).toJSON();
+      } catch {
+        return null;
+      }
+    }
+
+    const cred = await this.statusListCredentialModule.getStatusListCredential(identifier);
+
+    return cred?.toJSON ? cred.toJSON() : cred ?? null;
   }
 }
 
 /**
- * Resolves `StatusList2021Credential`s with identifier `status-list2021:dock:*`.
+ * Resolves `StatusList2021Credential`s with identifier `status-list2021:*` or `http(s)` URL.
  * @type {StatusListResolver}
  */
 export default StatusListResolver;
